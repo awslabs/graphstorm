96805945f871c5cab4cd4a08d76e10592b92bb89
Da Zheng <dzzhen@amazon.com>
2023-01-02 01:22:18 +0000

    Add more instructions on set up distributed regression tests

--- tests/regression-tests/rerun_gsf_docker.sh
+++ tests/regression-tests/rerun_gsf_docker.sh
@@ -3,4 +3,7 @@ if [ ! -z $docker_id ]; then
         docker kill $docker_id
         docker rm $docker_id
 fi
-nvidia-docker run -v /home/ubuntu/workspace/gsf-data/:/data -v /home/ubuntu/workspace/graph-storm/:/graph-storm -v /dev/shm:/dev/shm --network=host --name regression_test -d 911734752298.dkr.ecr.us-east-1.amazonaws.com/graphstorm_alpha:v3
+
+DATA_FOLDER=/home/ubuntu/workspace/gsf-data
+GS_HOME=/home/ubuntu/workspace/graph-storm
+nvidia-docker run -v $DATA_FOLDER/:/data -v $GS_HOME/:/graph-storm -v /dev/shm:/dev/shm --network=host --name regression_test -d 911734752298.dkr.ecr.us-east-1.amazonaws.com/graphstorm_alpha:v3
