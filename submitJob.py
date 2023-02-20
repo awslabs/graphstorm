45cd507b00d0fc6bb90982f49271f5ea7430860e
Prateek Mutalik Desai <pmdesai@amazon.com>
2023-02-20 12:37:08 -0800

    conditional test

--- submitJob.py
+++ submitJob.py
@@ -22,7 +22,7 @@ job_type_info = {
         'job_queue': 'graphstorm-queue-v1',
     },
     'CI-CPU-PUSH': {
-        'job_definition': 'temp-job-definition',
+        'job_definition': 'conditional-definition',
         'job_queue': 'graphstorm-queue-v1',
     },
     'CI-GPU-PUSH': {
