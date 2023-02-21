0b0b4eb7329eb37e62580cab7df54b28c0aaa3ba
Prateek Mutalik Desai <pmdesai@amazon.com>
2023-02-20 18:00:56 -0800

    adding more actions

--- submitJob.py
+++ submitJob.py
@@ -22,7 +22,7 @@ job_type_info = {
         'job_queue': 'graphstorm-queue-v1',
     },
     'CI-CPU-PUSH': {
-        'job_definition': 'conditional-definition',
+        'job_definition': 'test-definition',
         'job_queue': 'graphstorm-queue-v1',
     },
     'CI-GPU-PUSH': {
