c990ae209bb1fa055b0278ec07ceb9c6deff6777
xiang song(charlie.song) <classicxsong@gmail.com>
2023-02-24 10:57:14 -0800

    Remove SageMaker folder
    SageMaker support will come in the next release

--- sagemaker/examples/gsgnn_ec/infer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python3 graphstorm/sagemaker/launch_infer.py --image-url GRAPHSTORM_IMAGE_URL --role IAM_ROLE --graph-name movie-lens-100k --graph-data-s3 S3_PATH_TO_GRAPH_DATA --task-type "edge_classification" --model-artifact-s3 S3_PATH_TO_MODEL_TO_BE_LOAD --model-sub-path MODEL_CHECKPOINT_NAME --infer-yaml-s3 S3_PATH_TO_INFER_CONFIG --infer-yaml-name ml_ec.yaml --emb-s3-path S3_PATH_TO_UPLOAD_EMB --n-layers 2 --n-hidden 128 --backend gloo --batch-size 128 --fanout 10,5 --feat-name user:feat movie:feat
--- sagemaker/examples/gsgnn_lp/infer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python3 graphstorm/sagemaker/launch_infer.py --image-url GRAPHSTORM_IMAGE_URL --region us-east-1 --role IAM_ROLE --graph-name ogbn-arxiv --graph-data-s3 S3_PATH_TO_GRAPH_DATA --task-type "link_prediction" --model-artifact-s3 S3_PATH_TO_MODEL_TO_BE_LOAD --model-sub-path MODEL_CHECKPOINT_NAME --infer-yaml-s3 S3_PATH_TO_INFER_CONFIG --infer-yaml-name arxiv_lp_hf.yaml --emb-s3-path S3_PATH_TO_UPLOAD_EMB --n-layers 1 --n-hidden 128 --backend gloo --batch-size 128  --feat-name node:feat
