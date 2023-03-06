e83c6fa05e72206925615d328beb3e1090d3107f
Prateek Mutalik Desai <pmdesai@amazon.com>
2023-03-05 23:45:46 -0800

    Updated workflow scripts

--- .github/workflow_scripts/e2e_mgpu_check.sh
+++ .github/workflow_scripts/e2e_mgpu_check.sh
@@ -2,11 +2,8 @@
 cd ../../
 
 sh ./tests/end2end-tests/setup.sh
-
 sh ./tests/end2end-tests/create_data.sh
-
 sh ./tests/end2end-tests/graphstorm-lp/mgpu_test.sh
-
 sh ./tests/end2end-tests/graphstorm-nc/mgpu_test.sh
-
 sh ./tests/end2end-tests/graphstorm-ec/mgpu_test.sh
+
