e21d00cd2124d7769cce83d249acd054f9eeb6af
Da Zheng <zhengda@192.168.0.100>
2023-03-19 15:05:39 +0800

    fix.

--- tests/end2end-tests/data_process/test_data.py
+++ tests/end2end-tests/data_process/test_data.py
@@ -3,6 +3,7 @@ import dgl
 import pyarrow.parquet as pq
 import pyarrow as pa
 import numpy as np
+import torch as th
 
 out_dir = '/tmp/test_out/'
 conf_file = '/tmp/test_data/test_data_transform.conf'
@@ -19,9 +20,13 @@ reverse_node1_map = {val: key for key, val in zip(node1_map['orig'], node1_map['
 # Test the first node data
 data = g.nodes['node1'].data['feat'].numpy()
 label = g.nodes['node1'].data['label'].numpy()
+assert label.dtype == np.int32
 orig_ids = np.array([reverse_node1_map[new_id] for new_id in range(g.number_of_nodes('node1'))])
 assert np.all(data == orig_ids)
 assert np.all(label == orig_ids % 100)
+assert th.sum(g.nodes['node1'].data['train_mask']) == int(g.number_of_nodes('node1') * 0.8)
+assert th.sum(g.nodes['node1'].data['val_mask']) == int(g.number_of_nodes('node1') * 0.2)
+assert th.sum(g.nodes['node1'].data['test_mask']) == 0
 
 # Test the second node data
 data = g.nodes['node2'].data['feat'].numpy()
@@ -32,7 +37,13 @@ for i in range(data.shape[1]):
 
 # Test the edge data
 src_ids, dst_ids = g.edges(etype=('node1', 'relation1', 'node2'))
-label = g.edges[('node1', 'relation1', 'node2')].data['label']
+label = g.edges[('node1', 'relation1', 'node2')].data['label'].numpy()
+assert label.dtype == np.int32
 src_ids = np.array([reverse_node1_map[src_id] for src_id in src_ids.numpy()])
 dst_ids = dst_ids.numpy()
-assert np.all((src_ids + dst_ids) % 100 == label.numpy())
+assert np.all((src_ids + dst_ids) % 100 == label)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['train_mask']) \
+        == int(g.number_of_edges(('node1', 'relation1', 'node2')) * 0.8)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['val_mask']) \
+        == int(g.number_of_edges(('node1', 'relation1', 'node2')) * 0.2)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['test_mask']) == 0
