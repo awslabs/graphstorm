23651932b01732057330d43f73ec0b3430f7ca0a
Ubuntu <ubuntu@ip-172-31-11-221.us-west-2.compute.internal>
2023-04-04 00:14:42 +0000

    [Fix] fix a bug of using wrong num_negative_edge for test evaluation.

--- inference_scripts/lp_infer/lp_infer_lm.py
+++ inference_scripts/lp_infer/lp_infer_lm.py
@@ -67,7 +67,7 @@ def main(args):
 
     dataloader = test_dataloader_cls(infer_data, infer_data.test_idxs,
                                      batch_size=config.eval_batch_size,
-                                     num_negative_edges=config.num_negative_edges)
+                                     num_negative_edges=config.num_negative_edges_eval)
     # Preparing input layer for training or inference.
     # The input layer can pre-compute node features in the preparing step if needed.
     # For example pre-compute all BERT embeddings
