736a129db94ceaa8985c6599685ffd707352754c
Xiang Song <xiangsx@amazon.com>
2023-04-05 00:29:07 -0700

    update

--- python/graphstorm/trainer/ep_trainer.py
+++ python/graphstorm/trainer/ep_trainer.py
@@ -91,6 +91,7 @@ class GSgnnEdgePredictionTrainer(GSgnnTrainer):
         static_graph = freeze_input_layer_epochs == 0
         model = DistributedDataParallel(self._model, device_ids=[self.dev_id],
                                         output_device=self.dev_id,
+                                        find_unused_parameters=True,
                                         static_graph=static_graph)
         device = model.device
         data = train_loader.data
