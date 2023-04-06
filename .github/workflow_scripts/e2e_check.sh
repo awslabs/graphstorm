f2d0bac37c2a8fa53dfced8741cb4174b2e2bf6b
xiang song(charlie.song) <classicxsong@gmail.com>
2023-04-05 22:45:59 -0700

    [Bug Fix] Fix error code check of CI script (#64)
    If the output of training launch script is redirected to a file through
    `tee` command, the error code got from `$?` is from tee instead of the
    training script. We need to get the error code through
    `${PIPESTATUS[0]}`.
    
    *Description of changes:*
    Change integration test code to use `${PIPESTATUS[0]}` instead of `$?`
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Xiang Song <xiangsx@amazon.com>

--- .github/workflow_scripts/e2e_check.sh
+++ .github/workflow_scripts/e2e_check.sh
@@ -8,8 +8,8 @@ sh ./tests/end2end-tests/create_data.sh
 sh ./tests/end2end-tests/tools/test_mem_est.sh
 sh ./tests/end2end-tests/data_process/test.sh
 sh ./tests/end2end-tests/custom-gnn/run_test.sh
-sh ./tests/end2end-tests/graphstorm-nc/test.sh
-sh ./tests/end2end-tests/graphstorm-lp/test.sh
-sh ./tests/end2end-tests/graphstorm-ec/test.sh
-sh ./tests/end2end-tests/graphstorm-er/test.sh
+bash ./tests/end2end-tests/graphstorm-nc/test.sh
+bash ./tests/end2end-tests/graphstorm-lp/test.sh
+bash ./tests/end2end-tests/graphstorm-ec/test.sh
+bash ./tests/end2end-tests/graphstorm-er/test.sh
 
