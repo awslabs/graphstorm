5e704dc7e7e3a28f12a1ae419abfd97eca4ea25c
Da Zheng <zhengda1936@gmail.com>
2023-04-19 14:09:37 +0800

    Refactor the code for feature transformation and label construction (#93)
    *Description of changes:*
    This PR refactors the code for feature transformation and label
    construction. The rules are formally explained in README.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-15-191.us-west-2.compute.internal>
    Co-authored-by: xiang song(charlie.song) <classicxsong@gmail.com>

--- python/graphstorm/gconstruct/transform.py
+++ python/graphstorm/gconstruct/transform.py
@@ -16,22 +16,55 @@
     Generate example graph data using built-in datasets for node classifcation,
     node regression, edge classification and edge regression.
 """
-from transformers import BertTokenizer
-import torch as th
-import numpy as np
 
-class Tokenizer:
+import numpy as np
+import torch as th
+from transformers import BertTokenizer
+
+class FeatTransform:
+    """ The base class for feature transformation.
+
+    Parameters
+    ----------
+    col_name : str
+        The name of the column that contains the feature.
+    feat_name : str
+        The feature name used in the constructed graph.
+    """
+    def __init__(self, col_name, feat_name):
+        self._col_name = col_name
+        self._feat_name = feat_name
+
+    @property
+    def col_name(self):
+        """ The name of the column that contains the feature.
+        """
+        return self._col_name
+
+    @property
+    def feat_name(self):
+        """ The feature name.
+        """
+        return self._feat_name
+
+class Tokenizer(FeatTransform):
     """ A wrapper to a tokenizer.
 
     It is defined to process multiple strings.
 
     Parameters
     ----------
-    tokenizer : a tokenizer
+    col_name : str
+        The name of the column that contains the text features
+    feat_name : str
+        The prefix of the tokenized data
+    tokenizer : HuggingFace Tokenizer
+        a tokenizer
     max_seq_length : int
         The maximal length of the tokenization results.
     """
-    def __init__(self, tokenizer, max_seq_length):
+    def __init__(self, col_name, feat_name, tokenizer, max_seq_length):
+        super(Tokenizer, self).__init__(col_name, feat_name)
         self.tokenizer = tokenizer
         self.max_seq_length = max_seq_length
 
@@ -51,6 +84,7 @@ class Tokenizer:
         att_masks = []
         type_ids = []
         for s in strs:
+            assert isinstance(s, str), "The input of the tokenizer has to be a string."
             t = self.tokenizer(s, max_length=self.max_seq_length,
                                truncation=True, padding='max_length', return_tensors='pt')
             tokens.append(t['input_ids'])
@@ -58,28 +92,35 @@ class Tokenizer:
             # This can signficantly reduce memory consumption.
             att_masks.append(t['attention_mask'].to(th.int8))
             type_ids.append(t['token_type_ids'].to(th.int8))
-        return {'token_ids': th.cat(tokens, dim=0).numpy(),
-                'attention_mask': th.cat(att_masks, dim=0).numpy(),
-                'token_type_ids': th.cat(type_ids, dim=0).numpy()}
+        token_id_name = 'input_ids'
+        atten_mask_name = 'attention_mask'
+        token_type_id_name = 'token_type_ids'
+        return {token_id_name: th.cat(tokens, dim=0).numpy(),
+                atten_mask_name: th.cat(att_masks, dim=0).numpy(),
+                token_type_id_name: th.cat(type_ids, dim=0).numpy()}
 
-def parse_tokenize(op):
-    """ Parse the tokenization configuration
-
-    The parser returns a function that tokenizes text with HuggingFace tokenizer.
-    The tokenization function returns a dict of three Pytorch tensors.
-
-    Parameters
-    ----------
-    op : dict
-        The configuration for the operation.
-
-    Returns
-    -------
-    callable : a function to process the data.
+class Noop(FeatTransform):
+    """ This doesn't transform the feature.
     """
-    tokenizer = BertTokenizer.from_pretrained(op['bert_model'])
-    max_seq_length = int(op['max_seq_length'])
-    return Tokenizer(tokenizer, max_seq_length)
+
+    def __call__(self, feats):
+        """ This transforms the features.
+
+        Parameters
+        ----------
+        feats : Numpy array
+            The feature data
+
+        Returns
+        -------
+        dict : The key is the feature name, the value is the feature.
+        """
+        assert isinstance(feats, np.ndarray), \
+                f"The feature {self.feat_name} has to be NumPy array."
+        assert np.issubdtype(feats.dtype, np.integer) \
+                or np.issubdtype(feats.dtype, np.floating), \
+                f"The feature {self.feat_name} has to be integers or floats."
+        return {self.feat_name: feats}
 
 def parse_feat_ops(confs):
     """ Parse the configurations for processing the features
@@ -88,7 +129,6 @@ def parse_feat_ops(confs):
     {
         "feature_col":  ["<column name>", ...],
         "feature_name": "<feature name>",
-        "data_type":    "<feature data type>",
         "transform":    {"name": "<operator name>", ...}
     }
 
@@ -99,28 +139,31 @@ def parse_feat_ops(confs):
 
     Returns
     -------
-    list of tuple : The operations
+    list of FeatTransform : The operations that transform features.
     """
     ops = []
     assert isinstance(confs, list), \
             "The feature configurations need to be in a list."
     for feat in confs:
-        # TODO(zhengda) we will support data type in the future.
-        dtype = None
-        if 'transform' not in feat:
-            transform = None
-        else:
-            transform = feat['transform']
-            assert 'name' in transform, \
-                    "'name' must be defined in the transformation field."
-            if transform['name'] == 'tokenize_hf':
-                transform = parse_tokenize(transform)
-            else:
-                raise ValueError('Unknown operation: {}'.format(transform['name']))
-        feat_name = feat['feature_name'] if 'feature_name' in feat else None
         assert 'feature_col' in feat, \
                 "'feature_col' must be defined in a feature field."
-        ops.append((feat['feature_col'], feat_name, dtype, transform))
+        feat_name = feat['feature_name'] if 'feature_name' in feat else feat['feature_col']
+        if 'transform' not in feat:
+            transform = Noop(feat['feature_col'], feat_name)
+        else:
+            conf = feat['transform']
+            assert 'name' in conf, "'name' must be defined in the transformation field."
+            if conf['name'] == 'tokenize_hf':
+                assert 'bert_model' in conf, \
+                        "'tokenize_hf' needs to have the 'bert_model' field."
+                tokenizer = BertTokenizer.from_pretrained(conf['bert_model'])
+                assert 'max_seq_length' in conf, \
+                        "'tokenize_hf' needs to have the 'max_seq_length' field."
+                max_seq_length = int(conf['max_seq_length'])
+                transform = Tokenizer(feat['feature_col'], feat_name, tokenizer, max_seq_length)
+            else:
+                raise ValueError('Unknown operation: {}'.format(conf['name']))
+        ops.append(transform)
     return ops
 
 def process_features(data, ops):
@@ -133,31 +176,19 @@ def process_features(data, ops):
     ----------
     data : dict
         The data stored as a dict.
-    ops : list of tuples
-        The operations. Each tuple contains two elements. The first element
-        is the data name and the second element is a Python function
-        to process the data.
+    ops : list of FeatTransform
+        The operations that transform features.
 
     Returns
     -------
     dict : the key is the data name, the value is the processed data.
     """
     new_data = {}
-    for feat_col, feat_name, dtype, op in ops:
-        # If the transformation is defined on the feature.
-        if op is not None:
-            res = op(data[feat_col])
-            if isinstance(res, dict):
-                for key, val in res.items():
-                    new_data[key] = val
-            else:
-                new_data[feat_name] = res
-        # If the required data type is defined on the feature.
-        elif dtype is not None:
-            new_data[feat_name] = data[feat_col].astype(dtype)
-        # If no transformation is defined for the feature.
-        else:
-            new_data[feat_name] = data[feat_col]
+    for op in ops:
+        res = op(data[op.col_name])
+        assert isinstance(res, dict)
+        for key, val in res.items():
+            new_data[key] = val
     return new_data
 
 def get_valid_label_index(label):
@@ -183,62 +214,53 @@ def get_valid_label_index(label):
         raise ValueError("GraphStorm only supports label data of integers and float." + \
                          f"This label data has data type of {label.dtype}.")
 
-def process_labels(data, label_confs, is_node):
+class LabelProcessor:
     """ Process labels
 
     Parameters
     ----------
-    data : dict
-        The data stored as a dict.
-    label_conf : list of dict
-        The list of configs to construct labels.
-    is_node : bool
-        Whether or not to process labels on nodes.
-
-    Returns
-    -------
-    dict of tensors : labels (optional) and train/validation/test masks.
+    col_name : str
+        The column name that contains the labels.
+    label_name : str
+        The label name.
+    split_pct : list of int
+        The percentage of training, validation and test.
     """
-    assert len(label_confs) == 1, "We only support one label per node/edge type."
-    label_conf = label_confs[0]
-    assert 'task_type' in label_conf, "'task_type' must be defined in the label field."
-    if label_conf['task_type'] == 'classification':
-        assert 'label_col' in label_conf, \
-                "'label_col' must be defined in the label field."
-        label_col = label_conf['label_col']
-        label = data[label_col]
-        assert np.issubdtype(label.dtype, np.integer) \
-                or np.issubdtype(label.dtype, np.floating), \
-                "The labels for classification have to be integers."
-        valid_label_idx = get_valid_label_index(label)
-        label = np.int32(label)
-        num_samples = len(label)
-    elif label_conf['task_type'] == 'regression':
-        assert 'label_col' in label_conf, \
-                "'label_col' must be defined in the label field."
-        label_col = label_conf['label_col']
-        label = data[label_col]
-        valid_label_idx = get_valid_label_index(label)
-        num_samples = len(label)
-    else:
-        assert label_conf['task_type'] == 'link_prediction', \
-                "The task type must be classification, regression or link_prediction."
-        assert not is_node, "link_prediction task must be defined on edges."
-        label_col = label = None
-        valid_label_idx = None
-        # Any column in the data can define the number of samples in the data.
-        for val in data.values():
-            num_samples = len(val)
-            break
+    def __init__(self, col_name, label_name, split_pct):
+        self._col_name = col_name
+        self._label_name = label_name
+        self._split_pct = split_pct
 
-    if 'split_pct' in label_conf:
-        train_split, val_split, test_split = label_conf['split_pct']
+    @property
+    def col_name(self):
+        """ The column name that contains the label.
+        """
+        return self._col_name
+
+    @property
+    def label_name(self):
+        """ The label name.
+        """
+        return self._label_name
+
+    def data_split(self, get_valid_idx, num_samples):
+        """ Split the data
+
+        Parameters
+        ----------
+        get_valid_idx : callable
+            The function that returns the index of samples with valid labels.
+        num_samples : int
+            The total number of samples.
+
+        Returns
+        -------
+        a dict of tensors : the training, validation, test masks.
+        """
+        train_split, val_split, test_split = self._split_pct
         assert train_split + val_split + test_split <= 1, \
                 "The data split of training/val/test cannot be more than the entire dataset."
-        if valid_label_idx is None:
-            rand_idx = np.random.permutation(num_samples)
-        else:
-            rand_idx = np.random.permutation(valid_label_idx)
+        rand_idx = get_valid_idx()
         num_labels = len(rand_idx)
         num_train = int(num_labels * train_split)
         num_val = int(num_labels * val_split)
@@ -255,12 +277,152 @@ def process_labels(data, label_confs, is_node):
         train_mask[train_idx] = 1
         val_mask[val_idx] = 1
         test_mask[test_idx] = 1
-    if label_col is None:
-        return {'train_mask': train_mask,
-                'val_mask': val_mask,
-                'test_mask': test_mask}
+        train_mask_name = 'train_mask'
+        val_mask_name = 'val_mask'
+        test_mask_name = 'test_mask'
+        return {train_mask_name: train_mask,
+                val_mask_name: val_mask,
+                test_mask_name: test_mask}
+
+class ClassificationProcessor(LabelProcessor):
+    """ Process the label for the classification task.
+    """
+
+    def __call__(self, data):
+        """ Process the label for classification.
+
+        This performs data split on the nodes/edges and generates training/validation/test masks.
+
+        Parameters
+        ----------
+        data : dict of Tensors
+            All data associated with nodes/edges of a node/edge type.
+
+        Returns
+        -------
+        dict of Tensors : it contains the labels as well as training/val/test splits.
+        """
+        assert self.col_name in data, f"The label column {self.col_name} does not exist."
+        label = data[self.col_name]
+        assert np.issubdtype(label.dtype, np.integer) \
+                or np.issubdtype(label.dtype, np.floating), \
+                "The labels for classification have to be integers or floating points."
+        valid_label_idx = get_valid_label_index(label)
+        def permute_idx():
+            return np.random.permutation(valid_label_idx)
+        res = self.data_split(permute_idx, len(label))
+        res[self.label_name] = np.int32(label)
+        return res
+
+class RegressionProcessor(LabelProcessor):
+    """ Process the label for the regression task.
+    """
+
+    def __call__(self, data):
+        """ Process the label for regression.
+
+        This performs data split on the nodes/edges and generates training/validation/test masks.
+
+        Parameters
+        ----------
+        data : dict of Tensors
+            All data associated with nodes/edges of a node/edge type.
+
+        Returns
+        -------
+        dict of Tensors : it contains the labels as well as training/val/test splits.
+        """
+        assert self.col_name in data, f"The label column {self.col_name} does not exist."
+        label = data[self.col_name]
+        valid_label_idx = get_valid_label_index(label)
+        def permute_idx():
+            return np.random.permutation(valid_label_idx)
+        res = self.data_split(permute_idx, len(label))
+        res[self.label_name] = label
+        return res
+
+class LinkPredictionProcessor(LabelProcessor):
+    """ Process the label for the link prediction task.
+    """
+
+    def __call__(self, data):
+        """ Process the label for link prediction.
+
+        This performs data split on the edges and generates training/validation/test masks.
+
+        Parameters
+        ----------
+        data : dict of Tensors
+            All data associated with nodes/edges of a node/edge type.
+
+        Returns
+        -------
+        dict of Tensors : it contains training/val/test splits for link prediction.
+        """
+        # Any column in the data can define the number of samples in the data.
+        assert len(data) > 0, "The edge data is empty."
+        for val in data.values():
+            num_samples = len(val)
+            break
+        def permute_idx():
+            return np.random.permutation(num_samples)
+        return self.data_split(permute_idx, num_samples)
+
+def parse_label_ops(confs, is_node):
+    """ Parse the configurations to generate the label processor.
+
+    Parameters
+    ----------
+    confs : dict
+        Contain the configuration for labels.
+    is_node : bool
+        Whether the configurations are defined for nodes.
+
+    Returns
+    -------
+    list of LabelProcessor : the label processors generated from the configurations.
+    """
+    assert len(confs) == 1, "We only support one label per node/edge type."
+    label_conf = confs[0]
+    assert 'task_type' in label_conf, "'task_type' must be defined in the label field."
+    task_type = label_conf['task_type']
+    # By default, we use all labels for training.
+    if 'split_pct' in label_conf:
+        split_pct = label_conf['split_pct']
     else:
-        return {label_col: label,
-                'train_mask': train_mask,
-                'val_mask': val_mask,
-                'test_mask': test_mask}
+        print("'split_pct' is not found. " + \
+                "Use the default data split: train(80%), valid(10%), test(10%).")
+        split_pct = [0.8, 0.1, 0.1]
+
+    if task_type == 'classification':
+        assert 'label_col' in label_conf, \
+                "'label_col' must be defined in the label field."
+        label_col = label_conf['label_col']
+        return [ClassificationProcessor(label_col, label_col, split_pct)]
+    elif task_type == 'regression':
+        assert 'label_col' in label_conf, \
+                "'label_col' must be defined in the label field."
+        label_col = label_conf['label_col']
+        return [RegressionProcessor(label_col, label_col, split_pct)]
+    else:
+        assert task_type == 'link_prediction', \
+                "The task type must be classification, regression or link_prediction."
+        assert not is_node, "link_prediction task must be defined on edges."
+        return [LinkPredictionProcessor(None, None, split_pct)]
+
+def process_labels(data, label_processors):
+    """ Process labels
+
+    Parameters
+    ----------
+    data : dict
+        The data stored as a dict.
+    label_processors : list of LabelProcessor
+        The list of operations to construct labels.
+
+    Returns
+    -------
+    dict of tensors : labels (optional) and train/validation/test masks.
+    """
+    assert len(label_processors) == 1, "We only support one label per node/edge type."
+    return label_processors[0](data)
