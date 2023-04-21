c766023875979f97561522654531b2c0174b3788
Da Zheng <zhengda1936@gmail.com>
2023-04-21 17:30:24 +0800

    Support node data in separate files. (#104)
    *Description of changes:*
    In some cases, the node data of the same node type are stored in
    separate files. We need to use multiple node blocks to specify the
    files. Currently, we only support this for node data.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-15-191.us-west-2.compute.internal>

--- python/graphstorm/gconstruct/file_io.py
+++ python/graphstorm/gconstruct/file_io.py
@@ -24,6 +24,7 @@ import os
 import pyarrow.parquet as pq
 import pyarrow as pa
 import numpy as np
+import torch as th
 import h5py
 
 def read_data_json(data_file, data_fields):
@@ -140,6 +141,24 @@ def write_data_parquet(data, data_file):
     table = pa.Table.from_arrays(list(arr_dict.values()), names=list(arr_dict.keys()))
     pq.write_table(table, data_file)
 
+class HDF5Handle:
+    """ HDF5 file handle
+
+    This is to reference the HDF5 handle and close it when no one
+    uses the HDF5 file.
+
+    Parameters
+    ----------
+    f : HDF5 file handle
+        The handle to access the HDF5 file.
+    """
+    def __init__(self, f):
+        self._f = f
+
+    def __del__(self):
+        return self._f.close()
+
+
 class HDF5Array:
     """ This is an array wrapper class for HDF5 array.
 
@@ -150,12 +169,10 @@ class HDF5Array:
     ----------
     arr : HDF5 dataset
         The array-like object for accessing the HDF5 file.
-    hdf5_f : HDF5 file handle
-        The handle to access the HDF5 file.
     """
-    def __init__(self, arr, f):
+    def __init__(self, arr, handle):
         self._arr = arr
-        self._f = f
+        self._handle = handle
 
     def __len__(self):
         return self._arr.shape[0]
@@ -163,12 +180,11 @@ class HDF5Array:
     def __getitem__(self, idx):
         return self._arr[idx]
 
-    def __del__(self):
-        """ Destroy the object.
-
-        When the array is destroyed, we need to close the file automatically.
+    def to_tensor(self):
+        """ Return Pytorch tensor.
         """
-        return self._f.close()
+        arr = self._arr[:]
+        return th.tensor(arr)
 
     @property
     def shape(self):
@@ -202,10 +218,11 @@ def read_data_hdf5(data_file, data_fields=None, in_mem=True):
     """
     data = {}
     f = h5py.File(data_file, "r")
+    handle = HDF5Handle(f)
     data_fields = data_fields if data_fields is not None else f.keys()
     for name in data_fields:
         assert name in f, f"The data field {name} does not exist in the hdf5 file."
-        data[name] = f[name][:] if in_mem else HDF5Array(f[name], f)
+        data[name] = f[name][:] if in_mem else HDF5Array(f[name], handle)
     return data
 
 def write_data_hdf5(data, data_file):
@@ -241,8 +258,12 @@ def _parse_file_format(conf, is_node, in_mem):
     """
     fmt = conf["format"]
     assert 'name' in fmt, "'name' field must be defined in the format."
-    keys = [conf["node_id_col"]] if is_node \
-            else [conf["source_id_col"], conf["dest_id_col"]]
+    if is_node and "node_id_col" in conf:
+        keys = [conf["node_id_col"]]
+    elif is_node:
+        keys = []
+    else:
+        keys = [conf["source_id_col"], conf["dest_id_col"]]
     if "features" in conf:
         keys += [feat_conf["feature_col"] for feat_conf in conf["features"]]
     if "labels" in conf:
