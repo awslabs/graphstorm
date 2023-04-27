00a9034103082461437fdb7eed27ad2639e7d7f9
Da Zheng <zhengda1936@gmail.com>
2023-04-27 17:55:16 +0800

    create test data.

--- tests/end2end-tests/create_data.sh
+++ tests/end2end-tests/create_data.sh
@@ -5,104 +5,59 @@ mkdir -p /data
 cd /data
 cp -R /storage/ml-100k /data
 
-python3 $GS_HOME/tests/end2end-tests/data_process/process_movielens.py
+python3 $GS_HOME/tests/end2end-tests/data_gen/process_movielens.py
 
 # movielens node class with balanced training set
-python3 /$GS_HOME/tools/partition_graph.py --dataset movie-lens-100k \
-	--filepath /data \
-	--target_ntype movie \
-	--undirected \
-	--num_trainers_per_machine 4 \
-	--output movielen_100k_train_val_1p_4t \
-	--generate_new_node_split true \
-	--balance_train \
-	--balance_edges \
-	--num_parts 1
+python3 -m graphstorm.gconstruct.construct_graph \
+	--conf_file $GS_HOME/tests/end2end-tests/data_gen/movielens.json \
+	--num_processes 1 \
+	--output_dir movielen_100k_train_val_1p_4t \
+	--graph_name movie-lens-100k \
+	--add_reverse_edges
 
-python3 /$GS_HOME/tools/partition_graph.py --dataset movie-lens-100k-text \
-	--filepath /data \
-	--target_ntype movie \
-	--undirected \
-	--num_trainers_per_machine 4 \
-	--output movielen_100k_text_train_val_1p_4t \
-	--generate_new_node_split true \
-	--balance_train \
-	--balance_edges \
-	--num_parts 1
+python3 -m graphstorm.gconstruct.construct_graph \
+	--conf_file $GS_HOME/tests/end2end-tests/data_gen/movielens_text.json \
+	--num_processes 1 \
+	--output_dir movielen_100k_text_train_val_1p_4t \
+	--graph_name movie-lens-100k-text \
+	--add_reverse_edges
 
 # movielens link prediction
-python3 /$GS_HOME/tools/partition_graph_lp.py --dataset movie-lens-100k \
-	--filepath /data \
-	--target_etype "user,rating,movie" \
-	--num_trainers_per_machine 4 \
-	--output movielen_100k_lp_train_val_1p_4t \
-	--balance_train \
-	--balance_edges \
-	--train_pct 0.1 \
-	--val_pct 0.1 \
-	--num_parts 1
+python3 -m graphstorm.gconstruct.construct_graph \
+	--conf_file $GS_HOME/tests/end2end-tests/data_gen/movielens_lp.json \
+	--num_processes 1 \
+	--output_dir movielen_100k_lp_train_val_1p_4t \
+	--graph_name movie-lens-100k \
+	--add_reverse_edges
 
 # movielens link prediction with text features
-python3 /$GS_HOME/tools/partition_graph_lp.py --dataset movie-lens-100k-text  \
-	--filepath /data \
-	--target_etype "user,rating,movie" \
-	--num_trainers_per_machine 4 \
-	--output movielen_100k_text_lp_train_val_1p_4t \
-	--balance_train \
-	--balance_edges \
-	--train_pct 0.1 \
-	--val_pct 0.1 \
-	--num_parts 1
+#python3 -m graphstorm.gconstruct.construct_graph \
+#	--conf_file $GS_HOME/tests/end2end-tests/data_gen/movielens_lp_text.json \
+#	--num_processes 1 \
+#	--output_dir movielen_100k_text_lp_train_val_1p_4t \
+#	--graph_name movie-lens-100k-text \
+#	--add_reverse_edges
 
-# movielens node class
+# movielens link prediction without data split.
 rm -Rf movielen_no_edata_100k_train_val_1p_4t
 cp -R movielen_100k_lp_train_val_1p_4t movielen_no_edata_100k_train_val_1p_4t
 python3 /$GS_HOME/tests/end2end-tests/data_gen/remove_mask.py --dataset movielen_no_edata_100k_train_val_1p_4t --remove_node_mask 0
 
 # movielens edge regression
-python3 /$GS_HOME/tools/partition_graph.py --dataset movie-lens-100k \
-	--filepath /data \
-    --elabel_field "user,rating,movie:rate" \
-    --target_etype "user,rating,movie" \
-    --etask_type "regression" \
-	--num_trainers_per_machine 4 \
-	--output movielen_100k_er_1p_4t \
-	--balance_train \
-	--balance_edges \
-	--generate_new_edge_split true \
-	--train_pct 0.1 \
-	--val_pct 0.1 \
-	--num_parts 1
+python3 -m graphstorm.gconstruct.construct_graph \
+	--conf_file $GS_HOME/tests/end2end-tests/data_gen/movielens_er.json \
+	--num_processes 1 \
+	--output_dir movielen_100k_er_1p_4t \
+	--graph_name movie-lens-100k \
+	--add_reverse_edges
 
 # dummy data Edge Classification
-python3 /$GS_HOME/tools/partition_graph.py --dataset movie-lens-100k \
-	--filepath /data \
-    --elabel_field "user,rating,movie:rate" \
-    --target_etype "user,rating,movie" \
-    --etask_type "classification" \
-	--num_trainers_per_machine 4 \
-	--output movielen_100k_ec_1p_4t \
-	--balance_train \
-	--balance_edges \
-	--generate_new_edge_split true \
-	--train_pct 0.1 \
-	--val_pct 0.1 \
-	--num_parts 1
-
-# Create data for edge classification with text features
-python3 /$GS_HOME/tools/partition_graph.py --dataset movie-lens-100k-text \
-	--filepath /data \
-    --elabel_field "user,rating,movie:rate" \
-    --target_etype "user,rating,movie" \
-    --etask_type "classification" \
-	--num_trainers_per_machine 4 \
-	--output movielen_100k_ec_1p_4t_text \
-	--balance_train \
-	--balance_edges \
-	--generate_new_edge_split true \
-	--train_pct 0.1 \
-	--val_pct 0.1 \
-	--num_parts 1
+python3 -m graphstorm.gconstruct.construct_graph \
+	--conf_file $GS_HOME/tests/end2end-tests/data_gen/movielens.json \
+	--num_processes 1 \
+	--output_dir movielen_100k_ec_1p_4t \
+	--graph_name movie-lens-100k \
+	--add_reverse_edges
 
 rm -Rf /data/movielen_100k_multi_label_ec
 cp -R /data/movielen_100k_ec_1p_4t /data/movielen_100k_multi_label_ec
