dbb540ea000d1054be09ceaa3a17087f131b4caf
Ubuntu <ubuntu@ip-172-31-35-48.us-west-2.compute.internal>
2023-05-18 23:57:22 +0000

    clean conf.py commented lines

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
 html_theme = 'furo'
-# html_theme = 'classic'
 html_static_path = ['_static']
