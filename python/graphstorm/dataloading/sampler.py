72e95684ff5337a3bbe58b1a1847ce1d3de0fb59
Xiang Song <xiangsx@amazon.com>
2023-05-22 00:14:37 -0700

    Add localjoint negative sampler

--- python/graphstorm/dataloading/sampler.py
+++ python/graphstorm/dataloading/sampler.py
@@ -210,3 +210,44 @@ class JointUniform(object):
             pos_neg_tuple = _gen_neg_pair(pos_pairs,
                 g.canonical_etypes[0][0], g.canonical_etypes[0][2])
         return pos_neg_tuple
+
+class JointLocalUniform(JointUniform):
+    '''Jointly corrupt a group of edges.
+    The main idea is to sample a set of nodes and use them to corrupt all edges in a mini-batch.
+    This algorithm won't change the sampling probability for each individual edge, but can
+    significantly reduce the number of nodes in a mini-batch.
+
+    Parameters
+    ----------
+    k : int
+        The number of negative examples per edge.
+    '''
+    def __init__(self, k, per_trainer=False):
+        self._local_neg_nids = {}
+        self._per_trainer = per_trainer
+        super(LocalUniform, self).__init__(k)
+
+
+    def _generate(self, g, eids, canonical_etype):
+        _, _, vtype = canonical_etype
+        shape = eids.shape
+        dtype = eids.dtype
+        ctx = eids.device
+        src, _ = g.find_edges(eids, etype=canonical_etype)
+        src = np.repeat(src.numpy(), self.k)
+
+        if vtype not in self._local_neg_nids:
+            pb = g.get_partition_book()
+            if self._per_trainer:
+                neg_idx = node_split(th.full((g.num_nodes(vtype),), True, dtype=th.bool),
+                                     pb, ntype=vtype, force_even=False,
+                                     node_trainer_ids=g.nodes[vtype].data['trainer_id'])
+            else:
+                neg_idx = node_split(th.full((g.num_nodes(vtype),), True, dtype=th.bool),
+                                     pb, ntype=vtype, force_even=True)
+            self._local_neg_nids[vtype] = neg_idx
+
+        dst = th.randint(len(self._local_neg_nids[vtype]), (shape[0],), dtype=dtype, device=ctx)
+        dst = self._local_neg_nids[vtype][dst]
+        dst = np.tile(dst, self.k)
+        return th.as_tensor(src), th.as_tensor(dst)
