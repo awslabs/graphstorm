9f33d580607022ab9507f4a72260089a88e83bb7
kylasa <kylasa@gmail.com>
2023-06-08 16:39:52 -0700

    Fix import statement.

--- tests/unit-tests/graphpartitioning/graph_dataset.py
+++ tests/unit-tests/graphpartitioning/graph_dataset.py
@@ -23,7 +23,7 @@ import numpy as np
 import pyarrow as pa
 import torch
 
-from graphstorm.graphpartitioning import array_readwriter, constants
+import array_readwriter
 
 ################### Constants used in pipelines unit tests ####################
 
