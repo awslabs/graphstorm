ac4c216c88c6d918ddd44b381feedda2432b7445
Runjie <jalencato23pistons@gmail.com>
2023-07-13 17:25:12 +0000

    fix comment

--- python/graphstorm/model/rgcn_encoder.py
+++ python/graphstorm/model/rgcn_encoder.py
@@ -106,7 +106,7 @@ class RelGraphConvLayer(nn.Module):
         self.ngnn_activation = ngnn_activation
         self.ngnn = nn.ParameterList()
         for _ in range(0, self.num_ngnn_layers):
-            mlp_layer = nn.Parameter(th.Tensor(in_feat, out_feat))
+            mlp_layer = nn.Parameter(th.Tensor(out_feat, out_feat))
             nn.init.xavier_uniform_(mlp_layer, gain=nn.init.calculate_gain('relu'))
             self.ngnn.append(mlp_layer)
 
