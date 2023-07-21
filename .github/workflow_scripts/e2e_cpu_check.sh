f420c4f76626f6dcb177f3df54762630b2b6efa5
Prateek Mutalik Desai <pmdesai@amazon.com>
2023-07-21 14:37:24 -0700

    CI Infra for CPU Support

---
+++ .github/workflow_scripts/e2e_cpu_check.sh
@@ -0,0 +1,13 @@
+# Move to parent directory
+cd ../../
+
+set -ex
+
+sh ./tests/end2end-tests/setup.sh
+sh ./tests/end2end-tests/create_data.sh
+sh ./tests/end2end-tests/tools/test_mem_est.sh
+sh ./tests/end2end-tests/custom-gnn/run_test.sh
+bash ./tests/end2end-tests/graphstorm-nc/test.sh
+bash ./tests/end2end-tests/graphstorm-lp/test.sh
+bash ./tests/end2end-tests/graphstorm-ec/test.sh
+bash ./tests/end2end-tests/graphstorm-er/test.sh
