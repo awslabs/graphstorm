f20df5c0d403a4c6e62e5c300c11190c11878442
jalencato <jalencato23pistons@gmail.com>
2023-08-17 10:49:56 -0700

    Update python/graphstorm/config/argument.py
    Co-authored-by: xiang song(charlie.song) <classicxsong@gmail.com>

--- python/graphstorm/config/argument.py
+++ python/graphstorm/config/argument.py
@@ -586,7 +586,7 @@ class GSConfig:
         # pylint: disable=no-member
         if hasattr(self, "_input_activate"):
             assert self._input_activate in ["none", "relu"], \
-                "Use input activate flag for None and Relu"
+                "Use input activate flag 'none' for None and 'relu' for torch.nn.functional.relu"
             return self._input_activate
 
         # By default, return relu which will put relu activation funtion here
