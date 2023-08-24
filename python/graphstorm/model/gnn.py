776e0470e18a35062f8b9acb4661e93b7b123b71
Xiang Song <xiangsx@amazon.com>
2023-08-24 00:06:57 -0700

    update

--- python/graphstorm/model/gnn.py
+++ python/graphstorm/model/gnn.py
@@ -583,7 +583,7 @@ class GSgnnModel(GSgnnModelBase):    # pylint: disable=abstract-method
                            get_world_size())
         if get_rank() == 0:
             print('successfully save the model to ' + model_path)
-        print('Time on save model {}'.format(time.time() - start_save_t))
+            print('Time on save model {}'.format(time.time() - start_save_t))
 
     @property
     def node_input_encoder(self):
