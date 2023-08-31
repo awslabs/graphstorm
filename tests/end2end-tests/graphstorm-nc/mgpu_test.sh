8bc875a578d0d38b201a8ff33f77af210280bfcd
Ubuntu <ubuntu@ip-172-31-12-121.us-west-2.compute.internal>
2023-08-31 23:47:14 +0000

    fix test.

--- tests/end2end-tests/graphstorm-nc/mgpu_test.sh
+++ tests/end2end-tests/graphstorm-nc/mgpu_test.sh
@@ -25,54 +25,54 @@ error_and_exit () {
 }
 
 echo "**************dataset: MovieLens classification, RGCN layer: 1, node feat: fixed HF BERT, BERT nodes: movie, inference: mini-batch save model save emb node"
-python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml --save-model-path /data/gsgnn_nc_ml/ --topk-model-to-save 1 --save-embed-path /data/gsgnn_nc_ml/emb/ --num-epochs 3 --logging-file train_log.txt
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml --save-model-path /data/gsgnn_nc_ml/ --topk-model-to-save 1 --save-embed-path /data/gsgnn_nc_ml/emb/ --num-epochs 3 --logging-file /tmp/train_log.txt --logging-level debug
 
 error_and_exit ${PIPESTATUS[0]}
 
 # check prints
-cnt=$(grep "save_embed_path: /data/gsgnn_nc_ml/emb/" train_log.txt | wc -l)
+cnt=$(grep "save_embed_path: /data/gsgnn_nc_ml/emb/" /tmp/train_log.txt | wc -l)
 if test $cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have save_embed_path"
     exit -1
 fi
 
-cnt=$(grep "save_model_path: /data/gsgnn_nc_ml/" train_log.txt | wc -l)
+cnt=$(grep "save_model_path: /data/gsgnn_nc_ml/" /tmp/train_log.txt | wc -l)
 if test $cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have save_model_path"
     exit -1
 fi
 
-bst_cnt=$(grep "Best Test accuracy" train_log.txt | wc -l)
+bst_cnt=$(grep "Best Test accuracy" /tmp/train_log.txt | wc -l)
 if test $bst_cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have Best Test accuracy"
     exit -1
 fi
 
-cnt=$(grep "Test accuracy" train_log.txt | wc -l)
+cnt=$(grep "Test accuracy" /tmp/train_log.txt | wc -l)
 if test $cnt -lt $((1+$bst_cnt))
 then
     echo "We use SageMaker task tracker, we should have Test accuracy"
     exit -1
 fi
 
-bst_cnt=$(grep "Best Validation accuracy" train_log.txt | wc -l)
+bst_cnt=$(grep "Best Validation accuracy" /tmp/train_log.txt | wc -l)
 if test $bst_cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have Best Validation accuracy"
     exit -1
 fi
 
-cnt=$(grep "Validation accuracy" train_log.txt | wc -l)
+cnt=$(grep "Validation accuracy" /tmp/train_log.txt | wc -l)
 if test $cnt -lt $((1+$bst_cnt))
 then
     echo "We use SageMaker task tracker, we should have Validation accuracy"
     exit -1
 fi
 
-cnt=$(grep "Best Iteration" train_log.txt | wc -l)
+cnt=$(grep "Best Iteration" /tmp/train_log.txt | wc -l)
 if test $cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have Best Iteration"
@@ -86,52 +86,52 @@ then
     exit -1
 fi
 
-best_epoch=$(grep "successfully save the model to" train_log.txt | tail -1 | tr -d '\n' | tail -c 1)
+best_epoch=$(grep "successfully save the model to" /tmp/train_log.txt | tail -1 | tr -d '\n' | tail -c 1)
 echo "The best model is saved in epoch $best_epoch"
 
-rm train_log.txt
+rm /tmp/train_log.txt
 
 echo "**************dataset: Movielens, do inference on saved model"
-python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/inference_scripts/np_infer/ --num-trainers $NUM_INFERs --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_infer.yaml --use-mini-batch-infer false  --save-embed-path /data/gsgnn_nc_ml/infer-emb/ --restore-model-path /data/gsgnn_nc_ml/epoch-$best_epoch/ --save-prediction-path /data/gsgnn_nc_ml/prediction/ --logging-file log.txt
+python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/inference_scripts/np_infer/ --num-trainers $NUM_INFERs --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_infer.yaml --use-mini-batch-infer false  --save-embed-path /data/gsgnn_nc_ml/infer-emb/ --restore-model-path /data/gsgnn_nc_ml/epoch-$best_epoch/ --save-prediction-path /data/gsgnn_nc_ml/prediction/ --logging-file /tmp/log.txt
 
 error_and_exit ${PIPESTATUS[0]}
 
-cnt=$(grep "| Test accuracy" log.txt | wc -l)
+cnt=$(grep "| Test accuracy" /tmp/log.txt | wc -l)
 if test $cnt -ne 1
 then
     echo "We do test, should have test accuracy"
     exit -1
 fi
 
-bst_cnt=$(grep "Best Test accuracy" log.txt | wc -l)
+bst_cnt=$(grep "Best Test accuracy" /tmp/log.txt | wc -l)
 if test $bst_cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have Best Test accuracy"
     exit -1
 fi
 
-bst_cnt=$(grep "Best Validation accuracy" log.txt | wc -l)
+bst_cnt=$(grep "Best Validation accuracy" /tmp/log.txt | wc -l)
 if test $bst_cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have Best Validation accuracy"
     exit -1
 fi
 
-cnt=$(grep "Validation accuracy" log.txt | wc -l)
+cnt=$(grep "Validation accuracy" /tmp/log.txt | wc -l)
 if test $cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have Validation accuracy"
     exit -1
 fi
 
-cnt=$(grep "Best Iteration" log.txt | wc -l)
+cnt=$(grep "Best Iteration" /tmp/log.txt | wc -l)
 if test $cnt -lt 1
 then
     echo "We use SageMaker task tracker, we should have Best Iteration"
     exit -1
 fi
 
-rm log.txt
+rm /tmp/log.txt
 
 python3 $GS_HOME/tests/end2end-tests/check_infer.py --train_embout /data/gsgnn_nc_ml/emb/ --infer_embout /data/gsgnn_nc_ml/infer-emb/
 
@@ -147,17 +147,19 @@ python3 $GS_HOME/tests/end2end-tests/check_infer.py --train_embout /data/gsgnn_n
 error_and_exit $?
 
 echo "**************dataset: MovieLens classification, RGCN layer: 1, node feat: fixed HF BERT, BERT nodes: movie, inference: mini-batch save model save emb node, early stop"
-python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml --save-model-path /data/gsgnn_nc_ml/ --topk-model-to-save 3 --save-embed-path /data/gsgnn_nc_ml/emb/ --use-early-stop True --early-stop-burnin-rounds 2 -e 20 --early-stop-rounds 3 --early-stop-strategy consecutive_increase | tee exec.log
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml --save-model-path /data/gsgnn_nc_ml/ --topk-model-to-save 3 --save-embed-path /data/gsgnn_nc_ml/emb/ --use-early-stop True --early-stop-burnin-rounds 2 -e 20 --early-stop-rounds 3 --early-stop-strategy consecutive_increase --logging-file /tmp/exec.log --logging-level debug
 
 error_and_exit ${PIPESTATUS[0]}
 
 # check early stop
-cnt=$(cat exec.log | grep "Evaluation step" | wc -l)
+cnt=$(cat /tmp/exec.log | grep "Evaluation step" | wc -l)
 if test $cnt -eq 20
 then
 	echo "Early stop should work, but it didn't"
 	exit -1
 fi
+
+rm /tmp/exec.log
 
 if test $cnt -le 4
 then
@@ -175,7 +177,7 @@ fi
 rm -fr /data/gsgnn_nc_ml/*
 
 echo "**************dataset: MovieLens classification, RGCN layer: 1, node feat: BERT nodes: movie, user inference: mini-batch save model save emb node"
-python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_utext.yaml  --save-model-path /data/gsgnn_nc_ml_text/ --topk-model-to-save 1 --save-embed-path /data/gsgnn_nc_ml_text/emb/ --num-epochs 3 --logging-file train_log.txt
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_utext.yaml  --save-model-path /data/gsgnn_nc_ml_text/ --topk-model-to-save 1 --save-embed-path /data/gsgnn_nc_ml_text/emb/ --num-epochs 3 --logging-file /tmp/train_log.txt
 
 error_and_exit ${PIPESTATUS[0]}
 
@@ -186,24 +188,24 @@ then
     exit -1
 fi
 
-best_epoch=$(grep "successfully save the model to" train_log.txt | tail -1 | tr -d '\n' | tail -c 1)
+best_epoch=$(grep "successfully save the model to" /tmp/train_log.txt | tail -1 | tr -d '\n' | tail -c 1)
 echo "The best model is saved in epoch $best_epoch"
 
-rm train_log.txt
+rm /tmp/train_log.txt
 
 echo "**************dataset: Movielens, do inference on saved model, decoder: dot"
-python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/inference_scripts/np_infer/ --num-trainers $NUM_INFERs --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_text_infer.yaml --use-mini-batch-infer false   --save-embed-path /data/gsgnn_nc_ml_text/infer-emb/ --restore-model-path /data/gsgnn_nc_ml_text/epoch-$best_epoch/ --save-prediction-path /data/gsgnn_nc_ml_text/prediction/ --logging-file log.txt
+python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/inference_scripts/np_infer/ --num-trainers $NUM_INFERs --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_text_infer.yaml --use-mini-batch-infer false   --save-embed-path /data/gsgnn_nc_ml_text/infer-emb/ --restore-model-path /data/gsgnn_nc_ml_text/epoch-$best_epoch/ --save-prediction-path /data/gsgnn_nc_ml_text/prediction/ --logging-file /tmp/log.txt --logging-level debug
 
 error_and_exit ${PIPESTATUS[0]}
 
-cnt=$(grep "| Test accuracy" log.txt | wc -l)
+cnt=$(grep "| Test accuracy" /tmp/log.txt | wc -l)
 if test $cnt -ne 1
 then
     echo "We do test, should have test accuracy"
     exit -1
 fi
 
-rm log.txt
+rm /tmp/log.txt
 
 python3 $GS_HOME/tests/end2end-tests/check_infer.py --train_embout /data/gsgnn_nc_ml_text/emb/ --infer_embout /data/gsgnn_nc_ml_text/infer-emb/
 
@@ -211,7 +213,7 @@ error_and_exit $?
 rm -fr /data/gsgnn_nc_ml_text/*
 
 echo "**************dataset: MovieLens classification, RGCN layer: 1, node feat: BERT nodes: movie, user, with warmup inference: mini-batch save model save emb node"
-python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_utext.yaml --save-model-path /data/gsgnn_nc_ml_text/ --topk-model-to-save 1 --save-embed-path /data/gsgnn_nc_ml_text/emb/ --num-epochs 3 --freeze-lm-encoder-epochs 1 --logging-file train_log.txt
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_utext.yaml --save-model-path /data/gsgnn_nc_ml_text/ --topk-model-to-save 1 --save-embed-path /data/gsgnn_nc_ml_text/emb/ --num-epochs 3 --freeze-lm-encoder-epochs 1 --logging-file /tmp/train_log.txt
 
 error_and_exit  ${PIPESTATUS[0]}
 
@@ -222,24 +224,24 @@ then
     exit -1
 fi
 
-best_epoch=$(grep "successfully save the model to" train_log.txt | tail -1 | tr -d '\n' | tail -c 1)
+best_epoch=$(grep "successfully save the model to" /tmp/train_log.txt | tail -1 | tr -d '\n' | tail -c 1)
 echo "The best model is saved in epoch $best_epoch"
 
-rm train_log.txt
+rm /tmp/train_log.txt
 
 echo "**************dataset: Movielens, do inference on saved model, decoder: dot"
-python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/inference_scripts/np_infer/ --num-trainers $NUM_INFERs --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_text_infer.yaml --use-mini-batch-infer false --save-embed-path /data/gsgnn_nc_ml_text/infer-emb/ --restore-model-path /data/gsgnn_nc_ml_text/epoch-$best_epoch/ --save-prediction-path /data/gsgnn_nc_ml_text/prediction/ --logging-file log.txt
+python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/inference_scripts/np_infer/ --num-trainers $NUM_INFERs --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_text_infer.yaml --use-mini-batch-infer false --save-embed-path /data/gsgnn_nc_ml_text/infer-emb/ --restore-model-path /data/gsgnn_nc_ml_text/epoch-$best_epoch/ --save-prediction-path /data/gsgnn_nc_ml_text/prediction/ --logging-file /tmp/log.txt --logging-level debug
 
 error_and_exit ${PIPESTATUS[0]}
 
-cnt=$(grep "| Test accuracy" log.txt | wc -l)
+cnt=$(grep "| Test accuracy" /tmp/log.txt | wc -l)
 if test $cnt -ne 1
 then
     echo "We do test, should have test accuracy"
     exit -1
 fi
 
-rm log.txt
+rm /tmp/log.txt
 
 python3 $GS_HOME/tests/end2end-tests/check_infer.py --train_embout /data/gsgnn_nc_ml_text/emb/ --infer_embout /data/gsgnn_nc_ml_text/infer-emb/
 
