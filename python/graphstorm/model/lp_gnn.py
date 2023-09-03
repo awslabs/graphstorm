4f8f8c9b1c9c6249a01207cd9ab5438a37ea01bb
Xiang Song <xiangsx@amazon.com>
2023-09-02 22:01:22 -0700

    update

--- python/graphstorm/model/lp_gnn.py
+++ python/graphstorm/model/lp_gnn.py
@@ -77,6 +77,7 @@ class GSgnnLinkPredictionModel(GSgnnModel, GSgnnLinkPredictionModelInterface):
         super(GSgnnLinkPredictionModel, self).__init__()
         self.alpha_l2norm = alpha_l2norm
 
+    # pylint: disable=unused-argument
     def forward(self, blocks, pos_graph,
         neg_graph, node_feats, edge_feats,
         pos_edge_feats=None, neg_edge_feats=None, input_nodes=None):
