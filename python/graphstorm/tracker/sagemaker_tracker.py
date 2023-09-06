672c7d5ed605b46a1723d4ec85323ebf19067a01
Xiang Song <xiangsx@amazon.com>
2023-09-06 10:53:15 -0700

    Fix metric printing for complex metric value

--- python/graphstorm/tracker/sagemaker_tracker.py
+++ python/graphstorm/tracker/sagemaker_tracker.py
@@ -17,6 +17,7 @@
 """
 
 import logging
+import numbers
 
 from .graphstorm_tracker import GSTaskTrackerAbc
 
@@ -59,8 +60,10 @@ class GSSageMakerTaskTracker(GSTaskTrackerAbc):
             if metric_value is not None:
                 if isinstance(metric_value, str):
                     logging.info("Step %d | %s: %s", step, metric_name, metric_value)
-                else:
+                elif isinstance(metric_value, numbers.Number):
                     logging.info("Step %d | %s: %.4f", step, metric_name, metric_value)
+                else:
+                    logging.info("Step %d | %s: %s", step, metric_name, str(metric_value))
 
     def log_train_metric(self, metric_name, metric_value, step, force_report=False):
         """ Log train metric
