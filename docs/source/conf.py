13ab5815f5ff3497059398dd8d06341df53efea3
Ubuntu <ubuntu@ip-172-31-35-48.us-west-2.compute.internal>
2023-09-08 05:53:24 +0000

    test readthedocs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,5 +44,5 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'furo'
-html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
