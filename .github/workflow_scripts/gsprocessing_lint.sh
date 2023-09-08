2bfa41eeea7b6ec786e20b2a04b8274f957fb4d3
JalenCato <jalencato23pistons@gmail.com>
2023-09-08 17:59:19 +0000

    test

---
+++ .github/workflow_scripts/gsprocessing_lint.sh
@@ -0,0 +1,9 @@
+# Move to parent directory
+cd ../../
+
+set -ex
+
+python3 -m pip install --upgrade prospector pip
+FORCE_CUDA=1 python3 -m pip install -e '.[test]'  --no-build-isolation
+
+pylint --rcfile=./tests/lint/pylintrc ./graphstorm-processing/graphstorm_processing/
