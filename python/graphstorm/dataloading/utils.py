674029238f1b28d3731963de7e4f232a814b34e3
Zichen Wang <zichewan@amazon.com>
2023-09-12 20:10:31 +0000

    Speed up `flip_node_mask` for semi-supervised setting

--- python/graphstorm/dataloading/utils.py
+++ python/graphstorm/dataloading/utils.py
@@ -16,7 +16,6 @@
     Utils for data loading.
 """
 
-from copy import deepcopy
 import logging
 
 import torch as th
@@ -113,7 +112,7 @@ def modify_fanout_for_target_etype(g, fanout, target_etypes):
         edge_fanout_lis.append(edge_fanout_dic)
     return edge_fanout_lis
 
-def flip_node_mask(dist_tensor):
+def flip_node_mask(dist_tensor, indices):
     """ Flip the node mask (0->1; 1->0) and return a flipped mask.
         This is equivalent to the `~` operator for boolean tensors.
 
@@ -121,12 +120,13 @@ def flip_node_mask(dist_tensor):
         ----------
         dist_tensor: dgl.distributed.DistTensor
             The input mask
+        indices: torch.Tensor
+            The vector node IDs that belong to the local rank
 
         Returns
         -------
         flipped mask: dgl.distributed.DistTensor
     """
-    flipped_dist_tensor = deepcopy(dist_tensor)
-    for idx in range(dist_tensor.shape[0]):
-        flipped_dist_tensor[idx] = 1 - dist_tensor[idx]
+    flipped_dist_tensor = th.ones(dist_tensor.shape, dtype=dist_tensor.dtype)
+    flipped_dist_tensor[indices] = 1 - dist_tensor[indices]
     return flipped_dist_tensor
