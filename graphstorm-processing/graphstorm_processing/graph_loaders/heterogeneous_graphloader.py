764ee5757e4ef8ff063c8daac054044a10bcc061
jalencato <jalencato23pistons@gmail.com>
2023-09-22 11:05:20 -0700

    Update heterogeneous_graphloader.py

--- graphstorm-processing/graphstorm_processing/graph_loaders/heterogeneous_graphloader.py
+++ graphstorm-processing/graphstorm_processing/graph_loaders/heterogeneous_graphloader.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from .loader_base import GraphLoader
-
+import numpy
 
 class HeterogeneousGraphLoader(GraphLoader):
     """DGL Heterogeneous Graph Loader
