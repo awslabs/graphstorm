407eb78fed1d1e6e578daafd264863f7e0aeaebf
Xiang Song <xiangsx@amazon.com>
2023-09-25 11:12:04 -0700

    Add instructions to build a docker for GraphStorm-wholegraph on AWS instances with efa support

---
+++ docker/wholegraph/install-wholegraph.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+which cmake
+cmake --version
+
+
+git clone https://github.com/fmtlib/fmt.git  /opt/fmt
+cd /opt/fmt
+git checkout 9.1.0
+mkdir build && cd build
+cmake -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE ..
+make
+make install
+
+git clone https://github.com/gabime/spdlog.git  /opt/spdlog
+cd /opt/spdlog && mkdir build && cd build
+cmake .. && make -j
+cp libspdlog.a  /usr/lib/libspdlog.a
+export PYTHON=/usr/bin/python
+
+mkdir /opt/rapids/
+cd  /opt/rapids/
+git clone https://github.com/rapidsai/wholegraph.git -b refactoring
+cd /opt/rapids/wholegraph/
+pip install --upgrade pip
+pip install scikit-build
+export WHOLEGRAPH_CMAKE_CUDA_ARCHITECTURES="70-real;80-real;90"
+# fix a bug in CMakeList.txt when build pylibwholegraph
+old="import sysconfig; print(sysconfig.get_config_var('BINLIBDEST'))"
+string="import sysconfig; print(\"%s/%s\" % (sysconfig.get_config_var(\"LIBDIR\"), sysconfig.get_config_var(\"INSTSONAME\")))"
+sed -i "s|$old|$string|" /opt/rapids/wholegraph/python/pylibwholegraph/CMakeLists.txt
+PYTHON=python3 bash build.sh libwholegraph pylibwholegraph -v
