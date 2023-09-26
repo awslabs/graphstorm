1d65e654487a31e4d4152c60ed347878367fb1fd
Da Zheng <zhengda1936@gmail.com>
2023-09-26 08:35:54 -0700

    Fix a bug in restoring models in the inference script (#482)
    *Description of changes:*
    We should allow users to customize the model layers to load in the
    inference script.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-12-121.us-west-2.compute.internal>

--- python/graphstorm/run/gsgnn_np/np_infer_gnn.py
+++ python/graphstorm/run/gsgnn_np/np_infer_gnn.py
@@ -52,7 +52,8 @@ def main(config_args):
                                     node_feat_field=config.node_feat_name,
                                     label_field=config.label_field)
     model = gs.create_builtin_node_gnn_model(infer_data.g, config, train_task=False)
-    model.restore_model(config.restore_model_path)
+    model.restore_model(config.restore_model_path,
+                        model_layer_to_load=config.restore_model_layers)
     infer = GSgnnNodePredictionInferrer(model)
     infer.setup_device(device=device)
     if not config.no_validation:
