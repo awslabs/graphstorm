f0ae2449c9d55c2712c47d9d837a8c8d9b247b69
Xiang Song <xiangsx@amazon.com>
2023-11-06 12:20:29 -0800

    Update

--- python/graphstorm/config/argument.py
+++ python/graphstorm/config/argument.py
@@ -1782,6 +1782,9 @@ class GSConfig:
             assert self.lp_loss_func == BUILTIN_LP_LOSS_CONTRASTIVELOSS, \
                 "Use contrastive-loss-temp only when the loss function is " \
                 f"{BUILTIN_LP_LOSS_CONTRASTIVELOSS} loss."
+
+            assert self._contrastive_loss_temp > 0, \
+                "Contrastive loss temperature must be larger than 0"
             return self._contrastive_loss_temp
 
         return 1.0
