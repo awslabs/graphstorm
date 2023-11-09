c66d3a7080b68f1d77ebe6e5a8436e85352b58dc
Xiang Song <xiangsx@amazon.com>
2023-11-08 23:10:47 -0800

    Revert "Update"
    This reverts commit 9ff4f7a7ebfbab49df82c7e5a12fdae2deb7c05c.

--- tests/end2end-tests/graphstorm-nc/mgpu_test.sh
+++ tests/end2end-tests/graphstorm-nc/mgpu_test.sh
@@ -240,6 +240,15 @@ echo "The best model is saved in epoch $best_epoch"
 
 rm /tmp/train_log.txt
 
+echo "*************use cached LM embeddings"
+# Run the model training again and this time it should load the BERT embeddings saved
+# in the previous run.
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_utext.yaml --num-epochs 3 --logging-file /tmp/train_log.txt --cache-lm-embed true
+
+error_and_exit $?
+
+rm /tmp/train_log.txt
+
 echo "**************dataset: Movielens, do inference on saved model, RGCN layer: 1, node feat: BERT nodes: movie, user"
 python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/inference_scripts/np_infer/ --num-trainers $NUM_INFERs --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_text_infer.yaml --use-mini-batch-infer false   --save-embed-path /data/gsgnn_nc_ml_text/infer-emb/ --restore-model-path /data/gsgnn_nc_ml_text/epoch-$best_epoch/ --save-prediction-path /data/gsgnn_nc_ml_text/prediction/ --logging-file /tmp/log.txt --logging-level debug --preserve-input True
 
@@ -257,15 +266,6 @@ rm /tmp/log.txt
 python3 $GS_HOME/tests/end2end-tests/check_np_infer_emb.py --train-embout /data/gsgnn_nc_ml_text/emb/ --infer-embout /data/gsgnn_nc_ml_text/infer-emb/
 
 error_and_exit $?
-
-echo "*************use cached LM embeddings"
-# Run the model training again and this time it should load the BERT embeddings saved
-# in the previous run.
-python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lm_encoder_train_val_1p_4t/movie-lens-100k-text.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_utext.yaml --num-epochs 3 --logging-file /tmp/train_log.txt --cache-lm-embed true
-
-error_and_exit $?
-
-/tmp/train_log.txt
 
 # Run the model training again and this time it should load the BERT embeddings saved
 # in the previous run.
