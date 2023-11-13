01b0d0d15f7c6a10786e5059da19692cb0af8ab3
xiang song(charlie.song) <classicxsong@gmail.com>
2023-11-13 00:11:14 -0800

    Update GraphStorm SageMaker solution to support Node ID remapping. (#633)
    *Issue #, if available:*
    #508 
    
    *Description of changes:*
    Update SageMaker support to support automatic node ID remapping.
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Xiang Song <xiangsx@amazon.com>

--- python/graphstorm/config/__init__.py
+++ python/graphstorm/config/__init__.py
@@ -19,11 +19,12 @@
 from .argument import GSConfig
 from .argument import get_argument_parser
 
-from .config import BUILTIN_TASK_NODE_CLASSIFICATION
-from .config import BUILTIN_TASK_NODE_REGRESSION
-from .config import BUILTIN_TASK_EDGE_CLASSIFICATION
-from .config import BUILTIN_TASK_EDGE_REGRESSION
-from .config import BUILTIN_TASK_LINK_PREDICTION
+from .config import (BUILTIN_TASK_NODE_CLASSIFICATION,
+                     BUILTIN_TASK_NODE_REGRESSION,
+                     BUILTIN_TASK_EDGE_CLASSIFICATION,
+                     BUILTIN_TASK_EDGE_REGRESSION,
+                     BUILTIN_TASK_LINK_PREDICTION,
+                     BUILTIN_TASK_COMPUTE_EMB)
 from .config import SUPPORTED_TASKS
 
 from .config import BUILTIN_LP_DOT_DECODER
