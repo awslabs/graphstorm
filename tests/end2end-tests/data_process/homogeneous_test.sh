c69e7f0ff97b0bc613b54f81abded10a3bc785ea
JalenCato <jalencato23pistons@gmail.com>
2023-12-15 22:19:55 +0000

    add test

--- tests/end2end-tests/data_process/homogeneous_test.sh
+++ tests/end2end-tests/data_process/homogeneous_test.sh
@@ -45,6 +45,14 @@ echo "********* Test Node Classification on GConstruct Homogeneous Graph with re
 python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml --target-ntype _N
 error_and_exit $?
 
-echo "********* Test Edge Classification on GConstruct Homogeneous Graph with reverse edge ********"
+echo "********* Test Edge Classification on GConstruct Homogeneous Graph with homogeneous graph optimization********"
 python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_ec.yaml --target-etype _N,_E,_N
+error_and_exit $?
+
+echo "********* Test Node Classification with homogeneous graph optimization********"
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_homogeneous.yaml
+error_and_exit $?
+
+echo "********* Test Edge Classification with homogeneous graph optimization********"
+python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_ec_homogeneous.yaml
 error_and_exit $?
