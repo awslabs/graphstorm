411e220d0a4c6d3433ff33019cbcb11805a40b2d
jalencato <jalencato23pistons@gmail.com>
2023-12-19 17:01:11 -0800

    [Homo Optimization] Optimization on GSF (#686)
    *Issue #, if available:*
    
    *Description of changes:*
    
    Allow users to start training/inferring job without specifying
    target_ntype/target_etype on homogeneous graph.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: xiang song(charlie.song) <classicxsong@gmail.com>
    Co-authored-by: Xiang Song <xiangsx@amazon.com>

--- tests/end2end-tests/data_process/homogeneous_test.sh
+++ tests/end2end-tests/data_process/homogeneous_test.sh
@@ -45,6 +45,25 @@ echo "********* Test Node Classification on GConstruct Homogeneous Graph with re
 python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml --target-ntype _N
 error_and_exit $?
 
-echo "********* Test Edge Classification on GConstruct Homogeneous Graph with reverse edge ********"
+echo "********* Test Edge Classification on GConstruct Homogeneous Graph with reverse edge********"
 python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_ec.yaml --target-etype _N,_E,_N
 error_and_exit $?
+
+echo "********* Test Node Classification with homogeneous graph optimization********"
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_homogeneous.yaml --save-model-path /tmp/homogeneous_node_model
+error_and_exit $?
+
+echo "********* Test Node Classification with homogeneous graph optimization doing inference********"
+python3 -m graphstorm.run.gs_node_classification --inference --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc_homogeneous.yaml --restore-model-path /tmp/homogeneous_node_model/epoch-2
+error_and_exit $?
+
+echo "********* Test Edge Classification with homogeneous graph optimization********"
+python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_ec_homogeneous.yaml --save-model-path /tmp/homogeneous_edge_model
+error_and_exit $?
+
+echo "********* Test Edge Classification with homogeneous graph optimization doing inference********"
+python3 -m graphstorm.run.gs_edge_classification --inference --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /tmp/movielen_100k_train_val_1p_4t_homogeneous_rev/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_ec_homogeneous.yaml --restore-model-path /tmp/homogeneous_edge_model/epoch-2
+error_and_exit $?
+
+rm -rf /tmp/homogeneous_node_model
+rm -rf /tmp/homogeneous_edge_model
