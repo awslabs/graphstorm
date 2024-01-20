39e1c11e9d041a26df268aa26741624ca223b1dd
Ubuntu <ubuntu@ip-172-31-26-115.ec2.internal>
2024-01-20 21:28:54 +0000

    fix tests.

--- tests/end2end-tests/graphstorm-nc/test.sh
+++ tests/end2end-tests/graphstorm-nc/test.sh
@@ -37,6 +37,8 @@ rm -R ./models/movielen_100k/train_val/standalone
 echo "**************standalone"
 python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --cf ml_nc.yaml
 
+error_and_exit $?
+
 echo "**************dataset: MovieLens, RGCN layer: 1, node feat: fixed HF BERT, BERT nodes: movie, inference: mini-batch"
 python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml
 
