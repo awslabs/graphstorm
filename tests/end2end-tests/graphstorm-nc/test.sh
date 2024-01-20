49f4d9ed923a89cbe638962673eb564e2a7230ff
Da Zheng <zhengda1936@gmail.com>
2024-01-19 17:41:02 -0800

    add tests.

--- tests/end2end-tests/graphstorm-nc/test.sh
+++ tests/end2end-tests/graphstorm-nc/test.sh
@@ -34,6 +34,9 @@ error_and_exit $?
 
 rm -R ./models/movielen_100k/train_val/standalone
 
+echo "**************standalone"
+python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --cf ml_nc.yaml
+
 echo "**************dataset: MovieLens, RGCN layer: 1, node feat: fixed HF BERT, BERT nodes: movie, inference: mini-batch"
 python3 -m graphstorm.run.gs_node_classification --workspace $GS_HOME/training_scripts/gsgnn_np/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_nc.yaml
 
