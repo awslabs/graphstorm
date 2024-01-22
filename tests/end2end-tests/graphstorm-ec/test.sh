6312243ba931e939082d057d499cc7f380aa23ff
Ubuntu <ubuntu@ip-172-31-35-48.us-west-2.compute.internal>
2024-01-22 16:46:11 +0000

    fix imbalance weight error

--- tests/end2end-tests/graphstorm-ec/test.sh
+++ tests/end2end-tests/graphstorm-ec/test.sh
@@ -118,7 +118,7 @@ python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_s
 error_and_exit $?
 
 echo "**************dataset: Test edge classification, RGCN layer: 1, node feat: fixed HF BERT, BERT nodes: movie, inference: full-graph, imbalance-class"
-python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_ec_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222  --cf ml_ec.yaml --part-config /data/movielen_100k_ec_1p_4t/movie-lens-100k.json --use-mini-batch-infer false --imbalance-class-weights 1,1,2,1,2,1 --num-epochs 1
+python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_ec_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222  --cf ml_ec.yaml --part-config /data/movielen_100k_ec_1p_4t/movie-lens-100k.json --use-mini-batch-infer false --imbalance-class-weights 1,1,2,1,2 --num-epochs 1
 
 error_and_exit $?
 
