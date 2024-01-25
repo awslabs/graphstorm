f614d342150b942a38c564f6d0650b89eb5793c6
Jian Zhang (James) <6593865@qq.com>
2024-01-25 13:06:53 -0800

    Update python/graphstorm/eval/eval_func.py
    Co-authored-by: xiang song(charlie.song) <classicxsong@gmail.com>

--- python/graphstorm/eval/eval_func.py
+++ python/graphstorm/eval/eval_func.py
@@ -289,7 +289,7 @@ def compute_f1_score(y_preds, y_targets):
 
         Returns
         -------
-        The f1 score.
+        float: The f1 score.
     """
     y_true = y_targets.cpu().numpy()
     y_pred = y_preds.cpu().numpy()
