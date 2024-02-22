a0c4f9d4a2bb8b99b1470735cc6a1c7d918b25b3
JalenCato <jalencato23pistons@gmail.com>
2024-02-22 23:13:49 +0000

    apply black on all these

--- graphstorm-processing/tests/test_dist_noop_transformation.py
+++ graphstorm-processing/tests/test_dist_noop_transformation.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 from numpy.testing import assert_array_equal
 from pyspark.sql import functions as F, DataFrame, SparkSession
 
