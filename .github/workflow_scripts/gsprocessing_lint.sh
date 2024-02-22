3e04033ffe35ce54ebe22ffc4add41e8524d16bc
JalenCato <jalencato23pistons@gmail.com>
2024-02-22 23:14:25 +0000

    update

--- .github/workflow_scripts/gsprocessing_lint.sh
+++ .github/workflow_scripts/gsprocessing_lint.sh
@@ -7,4 +7,4 @@ pip install pylint==2.17.5
 pylint --rcfile=./tests/lint/pylintrc ./graphstorm-processing/graphstorm_processing/
 
 pip install black==24.2.0
-black --check ./graphstorm-processing/graphstorm_processing/
+black --check ./graphstorm-processing/
