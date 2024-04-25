781867c017d7aa85779cc15f033c3373bc153635
Xiang Song <xiangsx@amazon.com>
2024-04-25 11:59:25 -0700

    Update test

--- tests/end2end-tests/data_process/test_multitask_data.py
+++ tests/end2end-tests/data_process/test_multitask_data.py
@@ -66,3 +66,107 @@ elif args.graph_format == "DistDGL":
 else:
     raise ValueError('Invalid graph format: {}'.format(args.graph_format))
 
+node1_map = read_data_parquet(os.path.join(out_dir, "raw_id_mappings", "node1"))
+reverse_node1_map = {val: key for key, val in zip(node1_map['orig'], node1_map['new'])}
+node3_map = read_data_parquet(os.path.join(out_dir, "raw_id_mappings", "node3"))
+reverse_node3_map = {val: key for key, val in zip(node3_map['orig'], node3_map['new'])}
+
+# Test the first node data
+assert g.nodes['node1'].data['feat'].dtype is th.float32
+data = g.nodes['node1'].data['feat'].numpy()
+orig_ids = np.array([reverse_node1_map[new_id] for new_id in range(g.number_of_nodes('node1'))])
+# After graph construction, any 1D features will be converted to 2D features, so
+# here need to convert orig_ids to 2D to pass test
+np.testing.assert_allclose(data, orig_ids.reshape(-1, 1))
+
+label_class = g.nodes['node1'].data['label_class'].numpy()
+assert "train_mask_class" in g.nodes['node1'].data
+assert "val_mask_class" in g.nodes['node1'].data
+assert label_class.dtype == np.int32
+assert np.all(label_class == orig_ids % 100)
+assert np.all(np.nonzero(g.nodes['node1'].data['train_mask_class'].numpy()) == np.arange(100))
+assert np.all(np.nonzero(g.nodes['node1'].data['val_mask_class'].numpy()) == np.arange(100, 200))
+assert th.sum(g.nodes['node1'].data['test_mask_class']) == 0
+
+label_reg = g.nodes['node1'].data['label_reg'].numpy()
+assert label_reg.dtype == np.float32
+assert th.sum(g.nodes['node1'].data['train_mask_reg']) == int(g.number_of_nodes('node1') * 0.8)
+assert th.sum(g.nodes['node1'].data['val_mask_reg']) == int(g.number_of_nodes('node1') * 0.1)
+assert th.sum(g.nodes['node1'].data['test_mask_reg']) == int(g.number_of_nodes('node1') * 0.1)
+
+# Test the second node data
+label_class_1 = g.nodes['node2'].data['label_class1'].numpy()
+assert label_class_1.dtype == np.int32
+assert th.sum(g.nodes['node2'].data['train_mask_class1']) == int(g.number_of_nodes('node2') * 0.8)
+assert th.sum(g.nodes['node2'].data['val_mask_class1']) == int(g.number_of_nodes('node2') * 0.1)
+assert th.sum(g.nodes['node2'].data['test_mask_class1']) == int(g.number_of_nodes('node2') * 0.1)
+
+label_class_2 = g.nodes['node2'].data['label_class2'].numpy()
+assert label_class_1.dtype == np.int32
+assert th.sum(g.nodes['node2'].data['train_mask_class2']) == int(g.number_of_nodes('node2') * 0.8)
+assert th.sum(g.nodes['node2'].data['val_mask_class2']) == int(g.number_of_nodes('node2') * 0.1)
+assert th.sum(g.nodes['node2'].data['test_mask_class2']) == int(g.number_of_nodes('node2') * 0.1)
+
+# Test the third node data
+assert g.nodes['node3'].data['feat'].dtype is th.float32
+data = g.nodes['node3'].data['feat'].numpy()
+orig_ids = np.array([reverse_node3_map[new_id] for new_id in range(g.number_of_nodes('node3'))])
+np.testing.assert_allclose(data, np.repeat(orig_ids, 5).reshape(len(orig_ids), 5))
+
+# Test the edge data of edge type 1
+src_ids, dst_ids = g.edges(etype=('node1', 'relation1', 'node2'))
+assert 'label_class' in g.edges[('node1', 'relation1', 'node2')].data
+label_class = g.edges[('node1', 'relation1', 'node2')].data['label_class'].numpy()
+assert label_class.dtype == np.int32
+src_ids = np.array([reverse_node1_map[src_id] for src_id in src_ids.numpy()])
+dst_ids = dst_ids.numpy()
+assert np.all((src_ids + dst_ids) % 100 == label_class)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['train_mask_class']) \
+        == int(g.number_of_edges(('node1', 'relation1', 'node2')) * 0.8)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['val_mask_class']) \
+        == int(g.number_of_edges(('node1', 'relation1', 'node2')) * 0.2)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['test_mask_class']) == 0
+
+assert 'label_reg' in g.edges[('node1', 'relation1', 'node2')].data
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['train_mask_reg']) \
+        == int(g.number_of_edges(('node1', 'relation1', 'node2')) * 0.8)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['val_mask_reg']) \
+        == int(g.number_of_edges(('node1', 'relation1', 'node2')) * 0.1)
+assert th.sum(g.edges[('node1', 'relation1', 'node2')].data['test_mask_reg']) \
+        == int(g.number_of_edges(('node1', 'relation1', 'node2')) * 0.1)
+
+# Test the edge data of edge type 2
+assert th.sum(g.edges[('node1', 'relation2', 'node1')].data['train_mask']) \
+        == int(g.number_of_edges(('node1', 'relation2', 'node1')) * 0.8)
+assert th.sum(g.edges[('node1', 'relation2', 'node1')].data['val_mask']) \
+        == int(g.number_of_edges(('node1', 'relation2', 'node1')) * 0.2)
+assert th.sum(g.edges[('node1', 'relation2', 'node1')].data['test_mask']) == 0
+
+# Test hard negatives
+hard_neg = g.edges[('node1', 'relation2', 'node1')].data["hard_neg"]
+src_ids, dst_ids = g.edges(etype=("node1", "relation2", "node1"))
+ground_truth = th.cat((src_ids.reshape(-1,1), dst_ids.reshape(-1,1)), dim=1)
+assert th.sum(hard_neg-ground_truth) == 0
+
+# Test the edge data of edge type 3
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['train_mask_lp']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.8)
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['val_mask_lp']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.1)
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['test_mask_lp']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.1)
+
+assert 'label_class1' in g.edges[('node2', 'relation3', 'node3')].data
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['train_mask_class1']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.8)
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['val_mask_class1']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.2)
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['test_mask_class1']) == 0
+
+assert 'label_class2' in g.edges[('node2', 'relation3', 'node3')].data
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['train_mask_class2']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.8)
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['val_mask_class2']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.2)
+assert th.sum(g.edges[('node2', 'relation3', 'node3')].data['test_mask_class2']) \
+        == int(g.number_of_edges(('node2', 'relation3', 'node3')) * 0.1)
