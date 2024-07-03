c0b8501c5038309146e86bd0f6e9f8eef2f98d36
Xiang Song <xiangsx@amazon.com>
2024-07-02 17:38:04 -0700

    Update

--- python/graphstorm/gconstruct/transform.py
+++ python/graphstorm/gconstruct/transform.py
@@ -709,6 +709,7 @@ class RankGaussTransform(GlobalProcessFeatTransform):
 
             return {self.feat_name: feats}
 
+    """
     def after_merge_transform(self, feats):
         # The feats can be a numpy array or a numpy memmaped object
         # Get ranking information.
@@ -720,6 +721,28 @@ class RankGaussTransform(GlobalProcessFeatTransform):
         feats = (feats / feat_range - 0.5) * 2
         feats = np.clip(feats, -1 + self._epsilon, 1 - self._epsilon)
         feats = erfinv(feats)
+
+        return self.as_out_dtype(feats)
+    """
+
+    def after_merge_transform(self, feats):
+        # The feats can be a numpy array or a numpy memmaped object
+        # Get ranking information.
+        if isinstance(feats, ExtMemArrayWrapper):
+            feats = feats.to_numpy()
+        uni_feats, indices = np.unique(feats, axis=0, return_inverse=True)
+
+        uni_feats = uni_feats.argsort(axis=0).argsort(axis=0)
+        if len(uni_feats) == 1:
+            logging.warning(f"features of {self.feat_name} are identical."
+                            "Will return all 0s")
+            return self.as_out_dtype(np.zeros(feats.shape))
+
+        feat_range = len(uni_feats) - 1
+        uni_feats = (uni_feats / feat_range - 0.5) * 2
+        uni_feats = np.clip(uni_feats, -1 + self._epsilon, 1 - self._epsilon)
+        uni_feats = erfinv(uni_feats)
+        feats = uni_feats[indices]
 
         return self.as_out_dtype(feats)
 
