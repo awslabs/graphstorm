d5b1e036424e2403bcdbfdf30d6faac3df1e8d04
Theodore Vasiloudis <thvasilo@amazon.com>
2024-07-11 03:26:32 +0300

    [GSProcessing] Fix bug in repartition on leader, fix mypy errors and latent bugs (#915)
    *Issue #, if available:*
    
    *Description of changes:*
    * During the refactor with
    https://github.com/awslabs/graphstorm/pull/870 we moved the loader to be
    a class var for DistributedExecutor, but because the S3 path is not unit
    tested we missed on case where the output is on S3 and the user requests
    repartition on leader.
    * This error was actually picked up by mypy, I fixed some other
    potential issues and type annotations here.
    
    ### Testing
    
    Pre-commit, unit tests, and one test SageMaker job all succeed.
    
    The S3 codepath can only be integration tested currently.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- graphstorm-processing/graphstorm_processing/distributed_executor.py
+++ graphstorm-processing/graphstorm_processing/distributed_executor.py
@@ -313,6 +313,7 @@ class DistributedExecutor:
         )
 
         repartition_start = time.perf_counter()
+        updated_metadata = {}
         if all_match:
             logging.info(
                 "All file row counts match, applying Parquet metadata modification on Spark leader."
@@ -327,7 +328,7 @@ class DistributedExecutor:
                 try:
                     # Upload existing output files before trying to re-partition
                     if self.filesystem_type == FilesystemType.S3:
-                        self._upload_output_files(loader, force=True)
+                        self._upload_output_files(self.loader, force=True)
                     updated_metadata = repartition_files(graph_meta_dict, repartitioner)
                 except Exception as e:  # pylint: disable=broad-exception-caught
                     # If an error happens during re-partition, we don't want to fail the entire
