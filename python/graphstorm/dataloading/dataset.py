0fb92b17ce6d465c1274f19e55d2a51d30294760
Jian Zhang (James) <6593865@qq.com>
2024-07-29 15:31:03 -0700

    Update python/graphstorm/dataloading/dataset.py
    Co-authored-by: xiang song(charlie.song) <classicxsong@gmail.com>

--- python/graphstorm/dataloading/dataset.py
+++ python/graphstorm/dataloading/dataset.py
@@ -871,7 +871,7 @@ class GSgnnData():
         return idxs
 
     def get_edge_infer_set(self, etypes=None, mask="test_mask", reverse_edge_types_map=None):
-        """ Get inference set edges for the given edge types under the given mask.
+        """ Get the inference set for the given edge types under the given mask.
 
         If the mask exists in ``g.edges[etype].data``, the inference set
         is collected based on the mask.
