9bafcdfdceb394f1f330bef320e3daeffc6149ed
Jian Zhang (James) <6593865@qq.com>
2024-08-09 22:31:24 -0700

    [Doc] API doc string refactor for graphstorm.trainer/inferrer (#952)
    *Issue #, if available:*
    
    *Description of changes:*
    This PR refactors doc string and the index page of graphstorm.trainer
    and graphstorm.inference.
    
    For ready to review pages, here is the link.
    Trainer:
    https://james4graphstorm.readthedocs.io/en/james_apidoc_trif/api/references/graphstorm.trainer.html
    
    Inferrer:https://james4graphstorm.readthedocs.io/en/james_apidoc_trif/api/references/graphstorm.inference.html
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@ip-172-31-17-41.us-west-2.compute.internal>
    Co-authored-by: Theodore Vasiloudis <theodoros.vasiloudis@gmail.com>
    Co-authored-by: xiang song(charlie.song) <classicxsong@gmail.com>

--- docs/source/api/references/index.rst
+++ docs/source/api/references/index.rst
@@ -5,6 +5,8 @@ API Reference
 
 The released APIs include the necessary components that can build GraphStorm models, training and inference pipelines. For examples about how to use these APIs, please refer to the :ref:`API Programming Examples<programming-examples>`.
 
+.. note:: This API reference does not include the components for multi-task learning, which is still an experimental feature of Graphstorm v0.3, and may need extensive modification. 
+
 .. toctree::
    :maxdepth: 2
    :titlesonly:
--- python/graphstorm/trainer/np_trainer.py
+++ python/graphstorm/trainer/np_trainer.py
@@ -30,44 +30,45 @@ from ..utils import sys_tracker, rt_profiler, print_mem, get_rank
 from ..utils import barrier, is_distributed, get_backend
 
 class GSgnnNodePredictionTrainer(GSgnnTrainer):
-    """ A trainer for node prediction
+    """ Trainer for node prediction tasks.
 
-    This class is used to train models for node prediction tasks,
-    such as node classification and node regression.
+    ``GSgnnNodePredictionTrainer`` is used to train models for node prediction tasks,
+    such as node classification and node regression. ``GSgnnNodePredictionTrainer``
+    defines two main functions: 
 
-    It makes use of the functions provided by `GSgnnTrainer`
-    to define two main functions: `fit` that performs the training
-    for the model that is provided when the object is created,
-    and `eval` that evaluates a provided model against test and
-    validation data.
-
-    Parameters
-    ----------
-    model : GSgnnNodeModel
-        The GNN model for node prediction.
-    topk_model_to_save : int
-        The top K model to save. Default is 1.
+    * ``fit()``: performs the training for the model provided to this trainer
+      when the object is initialized, and;
+    * ``eval()``: evaluates the provided model against test and validation dataset.
 
     Example
-    -------
+    --------
 
     .. code:: python
 
         from graphstorm.dataloading import GSgnnNodeDataLoader
         from graphstorm.dataset import GSgnnData
-        from graphstorm.model.node_gnn import GSgnnNodeModel
+        from graphstorm.model import GSgnnNodeModel
         from graphstorm.trainer import GSgnnNodePredictionTrainer
 
-        my_dataset = GSgnnData("/path/to/part_config")
-        target_idx = {"my_node_type": target_nodes_tensor}
-        my_data_loader = GSgnnNodeDataLoader(
-            my_dataset, target_idx, fanout=[10], batch_size=1024,
-            label_field="label", node_feats="feat")
-        my_model = GSgnnNodeModel(alpha_l2norm=0.0)
+        np_data = GSgnnData("...")
+        target_idx = np_data.get_node_train_set('ntype1')
+        train_loader = GSgnnNodeDataLoader(
+            np_dataset, target_idx, fanout=[10], batch_size=1024,
+            label_field="label", node_feats="feat", train_task=True)
+        model = GSgnnNodeModel(alpha_l2norm=0.0)
 
-        trainer =  GSgnnNodePredictionTrainer(my_model, topk_model_to_save=1)
+        trainer = GSgnnNodePredictionTrainer(model)
 
-        trainer.fit(my_data_loader, num_epochs=2)
+        trainer.fit(train_loader, num_epochs=2)
+
+    Parameters
+    ----------
+    model: GSgnnNodeModelBase
+        The GNN model for node prediction, which could be a model class inherited from the
+        ``GSgnnNodeModelBase``, or a model class that inherits both the ``GSgnnModelBase``
+        and the ``GSgnnNodeModelInterface``.
+    topk_model_to_save: int
+        The top `K` model to be saved based on evaluation results. Default: 1.
     """
     def __init__(self, model, topk_model_to_save=1):
         super(GSgnnNodePredictionTrainer, self).__init__(model, topk_model_to_save)
@@ -84,43 +85,56 @@ class GSgnnNodePredictionTrainer(GSgnnTrainer):
             freeze_input_layer_epochs=0,
             max_grad_norm=None,
             grad_norm_type=2.0):
-        """ The fit function for node prediction.
+        """ Fit function for node prediction training.
 
-        Performs the training for `self.model`. Iterates over the training
-        batches in `train_loader` to compute the loss and perform the backwards
-        step using `self.optimizer`. If an evaluator has been assigned to the
-        trainer, it will run evaluation at the end of every epoch.
+        This function performs the training for the given node prediction model.
+        It iterates over the training batches provided by the ``train_loader``
+        to compute the loss, and then performs the backward steps using trainer's
+        own optimizer. 
+
+        If an evaluator and a validation dataloader are added to this trainer, during
+        training, the trainer will perform model evaluation in three cases:
+
+        * At the end of each epoch.
+        * At the evaluation frequency (number of iterations) defined in the evaluator.
+        * Before saving a model checkpoint.
 
         Parameters
         ----------
-        train_loader : GSgnnNodeDataLoader
-            The mini-batch sampler for training.
-        num_epochs : int
-            The max number of epochs to train the model.
-        val_loader : GSgnnNodeDataLoader
-            The mini-batch sampler for computing validation scores. The validation scores
-            are used for selecting models.
-        test_loader : GSgnnNodeDataLoader
-            The mini-batch sampler for computing test scores.
-        use_mini_batch_infer : bool
-            Whether or not to use mini-batch inference.
-        save_model_path : str
-            The path where the model is saved.
-        save_model_frequency : int
-            The number of iteration to train the model before saving the model. Default is -1,
-            meaning only save model after each epoch.
-        save_perf_results_path : str
-            The path of the file where the performance results are saved.
+        train_loader: GSgnnNodeDataLoader
+            Node dataloader for mini-batch sampling the training set.
+        num_epochs: int
+            The max number of epochs used to train the model.
+        val_loader: GSgnnNodeDataLoader
+            Node dataloader for mini-batch sampling the validation set. Default: None.
+        test_loader: GSgnnNodeDataLoader
+            Node dataloader for mini-batch sampling the test set. Default: None.
+        use_mini_batch_infer: bool
+            Whether to use mini-batch for inference. Default: True.
+        save_model_path: str
+            The path where trained model checkpoints are saved. If is None, will not
+            save model checkpoints.
+            Default: None.
+        save_model_frequency: int
+            The number of iterations to train the model before saving a model checkpoint. 
+            Default: -1, meaning only save model after each epoch.
+        save_perf_results_path: str
+            The path of the file where the performance results are saved. Default: None.
         freeze_input_layer_epochs: int
-            Freeze the input layer for N epochs. This is commonly used when
-            the input layer contains language models.
-            Default: 0, no freeze.
+            The number of epochs to freeze the input layer from updating trainable
+            parameters. This is commonly used when the input layer contains language models.
+            Default: 0.
         max_grad_norm: float
-            Clip the gradient by the max_grad_norm to ensure stability.
-            Default: None, no clip.
+            A value used to clip the gradient, which can enhance training stability.
+            More explanation of this argument can be found
+            in `torch.nn.utils.clip_grad_norm_ <https://pytorch.org/docs/2.1/generated/
+            torch.nn.utils.clip_grad_norm_.html#torch.nn.utils.clip_grad_norm_>`__.
+            Default: None.
         grad_norm_type: float
-            Norm type for the gradient clip
-            Default: 2.0
+            Norm type for the gradient clip. More explanation of this argument can be found
+            in `torch.nn.utils.clip_grad_norm_ <https://pytorch.org/docs/2.1/generated/
+            torch.nn.utils.clip_grad_norm_.html#torch.nn.utils.clip_grad_norm_>`__.
+            Default: 2.0.
         """
         # Check the correctness of configurations.
         if self.evaluator is not None:
@@ -285,26 +299,30 @@ class GSgnnNodePredictionTrainer(GSgnnTrainer):
 
     def eval(self, model, val_loader, test_loader, use_mini_batch_infer, total_steps,
              return_proba=True):
-        """ do the model evaluation using validation and test sets
+        """ Do model evaluation using the validation set, or test set if provided.
 
         Parameters
         ----------
-        model : Pytorch model
-            The GNN model.
-        val_loader: GSNodeDataLoader
-            The dataloader for validation data
-        test_loader : GSNodeDataLoader
-            The dataloader for test data.
+        model: GSgnnNodeModelBase
+            The GNN model for node prediction, which could be a model class inherited from the
+            ``GSgnnNodeModelBase``, or a model class that inherits both the ``GSgnnModelBase``
+            and the ``GSgnnNodeModelInterface``.
+        val_loader: GSgnnNodeDataLoader
+            Node dataloader for mini-batch sampling the validation set. Default: None.
+        test_loader: GSgnnNodeDataLoader
+            Node dataloader for mini-batch sampling the test set. Default: None.
         use_mini_batch_infer: bool
-            Whether do mini-batch inference
+            Whether to use mini-batch for inference. Default: True.
         total_steps: int
-            Total number of iterations.
+            The total number of iterations.
         return_proba: bool
-            Whether to return all the predictions or the maximum prediction.
+            Whether to return the prediction results or the argmax results for
+            classification tasks.
 
         Returns
         -------
-        float: validation score
+        val_score: dict
+            Validation scores of differnet metrics in the format of {metric: val_score}.
         """
         teval = time.time()
         sys_tracker.check('before prediction')
