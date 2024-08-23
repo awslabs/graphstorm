fbe3cf986cc60224a2514c1f2c17f6703a4ebd62
Xiang Song <xiangsx@amazon.com>
2024-08-23 10:38:12 -0700

    Update

--- tests/end2end-tests/graphstorm-lp/mgpu_test.sh
+++ tests/end2end-tests/graphstorm-lp/mgpu_test.sh
@@ -843,10 +843,12 @@ then
 fi
 
 rm /tmp/train_log.txt
+rm -fr /data/gsgnn_lp_ml_rotate/*
 
 echo "**************dataset: Movielens, two training edges but only one with edge weight for loss, score func: RotatE ***********"
-python3 -m graphstorm.run.gs_link_prediction --workspace $GS_HOME/training_scripts/gsgnn_lp --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lp_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_lp.yaml --fanout '10,15' --num-layers 2 --use-mini-batch-infer false --eval-batch-size 1024 --topk-model-to-save 1 --save-model-frequency 1000 --train-etype user,rating,movie movie,rating-rev,user --lp-edge-weight-for-loss user,rating,movie:rate --lp-decoder-type rotate
+python3 -m graphstorm.run.gs_link_prediction --workspace $GS_HOME/training_scripts/gsgnn_lp --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lp_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_lp.yaml --fanout '10,15' --num-layers 2 --use-mini-batch-infer false --eval-batch-size 1024 --topk-model-to-save 1 --save-model-frequency 1000 --train-etype user,rating,movie movie,rating-rev,user --lp-edge-weight-for-loss user,rating,movie:rate --lp-decoder-type rotate --save-model-path /data/gsgnn_lp_ml_rotate/
 
 error_and_exit $?
+rm -fr /data/gsgnn_lp_ml_rotate/*
 
 rm -fr /tmp/*
