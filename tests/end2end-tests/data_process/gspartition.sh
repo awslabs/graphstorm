11fd64292ac6297297aa88cb58f260dc2e9471d6
jalencato <jalencato23pistons@gmail.com>
2024-09-05 12:37:49 -0700

    Update gspartition.sh

--- tests/end2end-tests/data_process/gspartition.sh
+++ tests/end2end-tests/data_process/gspartition.sh
@@ -20,4 +20,4 @@ cat /ip_list.txt
 
 # Test the DGLGraph format.
 echo "********* Test the GSPartition *********"
-python3 -m graphstorm.gpartition.dist_partition_graph --metadata-filename updated_row_counts_metadata.json --input-path /gsp-output --output-path /gspartition-out --num-parts 2 --partition-algorithm random --ssh-port 2222 --ip-config /ip_file.txt
+python3 -m graphstorm.gpartition.dist_partition_graph --metadata-filename updated_row_counts_metadata.json --input-path /gsp-output --output-path /gspartition-out --num-parts 2 --partition-algorithm random --ssh-port 2222 --ip-config /ip_list.txt
