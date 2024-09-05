56069b7fd3c9c0ecd2da84485802c11f12e4c4fe
jalencato <jalencato23pistons@gmail.com>
2024-09-05 16:47:35 -0700

    Update test.sh

--- tests/end2end-tests/graphstorm-ec/test.sh
+++ tests/end2end-tests/graphstorm-ec/test.sh
@@ -27,11 +27,6 @@ echo "Test GraphStorm edge classification"
 
 date
 
-echo "**************standalone"
-python3 $GS_HOME/python/graphstorm/run/gsgnn_ep/gsgnn_ep.py --part-config /data/movielen_100k_ec_1p_4t/movie-lens-100k.json --cf $GS_HOME/training_scripts/gsgnn_ep/ml_ec.yaml
-
-error_and_exit $?
-
 echo "**************dataset: Test edge classification, RGCN layer: 1, node feat: fixed HF BERT, BERT nodes: movie, inference: mini-batch"
 python3 -m graphstorm.run.gs_edge_classification --workspace $GS_HOME/training_scripts/gsgnn_ep/ --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_ec_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_ec.yaml --num-epochs 1
 
