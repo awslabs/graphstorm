2a5577b3804683c29d1619cf166ae786716300e8
jalencato <jalencato23pistons@gmail.com>
2024-09-06 15:12:09 -0700

    Update e2e_mgpu_check.sh

--- .github/workflow_scripts/e2e_mgpu_check.sh
+++ .github/workflow_scripts/e2e_mgpu_check.sh
@@ -5,8 +5,8 @@ pip install --no-cache-dir --extra-index-url https://pypi.nvidia.com pylibwholeg
 
 set -ex
 
-bash ./tests/end2end-tests/setup.sh
-bash ./tests/end2end-tests/create_data.sh
+sh ./tests/end2end-tests/setup.sh
+sh ./tests/end2end-tests/create_data.sh
 bash ./tests/end2end-tests/graphstorm-nc/mgpu_test.sh
 bash ./tests/end2end-tests/graphstorm-ec/mgpu_test.sh
 
