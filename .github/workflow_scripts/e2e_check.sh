cf9d569b8fd8f74944ac6b3ed897fd066d390c10
JalenCato <jalencato23pistons@gmail.com>
2024-09-06 21:20:59 +0000

    sh to bash

--- .github/workflow_scripts/e2e_check.sh
+++ .github/workflow_scripts/e2e_check.sh
@@ -3,13 +3,13 @@ cd ../../
 
 set -ex
 
-sh ./tests/end2end-tests/setup.sh
-sh ./tests/end2end-tests/create_data.sh
-sh ./tests/end2end-tests/tools/test_mem_est.sh
-sh ./tests/end2end-tests/data_process/test.sh
-sh ./tests/end2end-tests/data_process/movielens_test.sh
-sh ./tests/end2end-tests/data_process/homogeneous_test.sh
-sh ./tests/end2end-tests/custom-gnn/run_test.sh
+bash ./tests/end2end-tests/setup.sh
+bash ./tests/end2end-tests/create_data.sh
+bash ./tests/end2end-tests/tools/test_mem_est.sh
+bash ./tests/end2end-tests/data_process/test.sh
+bash ./tests/end2end-tests/data_process/movielens_test.sh
+bash ./tests/end2end-tests/data_process/homogeneous_test.sh
+bash ./tests/end2end-tests/custom-gnn/run_test.sh
 bash ./tests/end2end-tests/graphstorm-nc/test.sh
 bash ./tests/end2end-tests/graphstorm-lp/test.sh
 bash ./tests/end2end-tests/graphstorm-ec/test.sh
