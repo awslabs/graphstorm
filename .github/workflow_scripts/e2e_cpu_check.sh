a54ed7823711b73f08e4452e4eef2fbdbdeaceba
jalencato <jalencato23pistons@gmail.com>
2024-09-06 15:11:57 -0700

    Update e2e_cpu_check.sh

--- .github/workflow_scripts/e2e_cpu_check.sh
+++ .github/workflow_scripts/e2e_cpu_check.sh
@@ -3,9 +3,9 @@ cd ../../
 
 set -ex
 
-bash ./tests/end2end-tests/setup.sh
-bash ./tests/end2end-tests/create_data.sh
-bash ./tests/end2end-tests/custom-gnn/run_test.sh
+sh ./tests/end2end-tests/setup.sh
+sh ./tests/end2end-tests/create_data.sh
+sh ./tests/end2end-tests/custom-gnn/run_test.sh
 bash ./tests/end2end-tests/graphstorm-nc/test-cpu.sh
 bash ./tests/end2end-tests/graphstorm-lp/test-cpu.sh
 bash ./tests/end2end-tests/graphstorm-ec/test-cpu.sh
