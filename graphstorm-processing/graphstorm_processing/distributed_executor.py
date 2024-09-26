e788214a8733d78024c27359b1b0ccb0a73a4cc7
xiang song(charlie.song) <classicxsong@gmail.com>
2024-09-26 13:23:48 -0700

    Update graphstorm-processing/graphstorm_processing/distributed_executor.py
    Co-authored-by: Theodore Vasiloudis <theodoros.vasiloudis@gmail.com>

--- graphstorm-processing/graphstorm_processing/distributed_executor.py
+++ graphstorm-processing/graphstorm_processing/distributed_executor.py
@@ -592,7 +592,7 @@ def check_graph_name(graph_name):
         "GraphStorm expects the graph name adheres to the Python"
         "identifier naming rules with the exception that hyphens "
         "(-) are permitted and the name can start with numbers. "
-        f"But we get {graph_name}"
+        f"Got: {graph_name}"
     )
 
 
