bb6c4817c6c166bac2294b0ae1f04bc78c5c11f7
xiang song(charlie.song) <classicxsong@gmail.com>
2024-10-03 10:22:03 -0700

    Update python/graphstorm/model/loss_func.py
    Co-authored-by: Jian Zhang (James) <6593865@qq.com>

--- python/graphstorm/model/loss_func.py
+++ python/graphstorm/model/loss_func.py
@@ -27,7 +27,7 @@ from .utils import get_rank
 class ClassifyLossFunc(GSLayer):
     """ Loss function for classification tasks.
 
-    If multilabel is set to True, the torch.nn.BCEWithLogitsLoss
+    If multilabel is set to True, the ``torch.nn.BCEWithLogitsLoss``
     is used, otherwise the torch.nn.CrossEntropyLoss is used.
 
     Parameters
