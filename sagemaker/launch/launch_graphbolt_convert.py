d1f96bb5776fc3500fa7f161e1b1287966299b4a
Theodore Vasiloudis <thvasilo@amazon.com>
2024-11-07 18:32:19 +0000

    Auto-format by black, replace print with logging, fix docs

--- sagemaker/launch/launch_graphbolt_convert.py
+++ sagemaker/launch/launch_graphbolt_convert.py
@@ -1,23 +1,43 @@
-""" Launch SageMaker training task
 """
+    Copyright Contributors
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+    Launch SageMaker GraphBolt conversion step
+"""
+
 import logging
 import os
+from pprint import pformat
 from time import strftime, gmtime
 
-import boto3 # pylint: disable=import-error
+import boto3  # pylint: disable=import-error
 import sagemaker
 from sagemaker.processing import ScriptProcessor
 from sagemaker.workflow.steps import ProcessingInput
 
-from common_parser import ( # pylint: disable=wrong-import-order
-    get_common_parser, parse_estimator_kwargs)
+from common_parser import (  # pylint: disable=wrong-import-order
+    get_common_parser,
+    parse_estimator_kwargs,
+)
 
 INSTANCE_TYPE = "ml.m5.12xlarge"
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 
+
 def run_gbconvert_job(input_args, image):
-    """ Run job using SageMaker estimator.PyTorch
+    """Run job using SageMaker estimator.PyTorch
 
         TODO: We may need to simplify the argument list. We can use a config object.
 
@@ -28,21 +48,24 @@ def run_gbconvert_job(input_args, image):
     image: str
         ECR image uri
     """
-    timestamp = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     # SageMaker base job name
-    sm_task_name = input_args.task_name if input_args.task_name else timestamp
-    role = input_args.role # SageMaker ARN role
-    instance_type = input_args.instance_type # SageMaker instance type
-    region = input_args.region # AWS region
-    graph_data_s3 = input_args.graph_data_s3 # S3 location storing input graph data (unpartitioned)
-    graph_data_s3 = graph_data_s3[:-1] if graph_data_s3[-1] == '/' \
-        else graph_data_s3 # The input will be an S3 prefix without trailing /
+    sm_task_name = input_args.task_name if input_args.task_name else "gs-gb-convert"
+    role = input_args.role  # SageMaker ARN role
+    instance_type = input_args.instance_type  # SageMaker instance type
+    region = input_args.region  # AWS region
+    graph_data_s3 = (
+        input_args.graph_data_s3
+    )  # S3 location storing input graph data (unpartitioned)
+    graph_data_s3 = (
+        graph_data_s3[:-1] if graph_data_s3[-1] == "/" else graph_data_s3
+    )  # The input will be an S3 prefix without trailing /
 
     sagemaker_session = sagemaker.Session(boto3.Session(region_name=region))
 
-    print(f"Parameters {input_args}")
+    logging.info("Parameters %s", pformat(input_args))
     if input_args.sm_estimator_parameters:
-        print(f"SageMaker Estimator parameters: '{input_args.sm_estimator_parameters}'")
+        logging.info("SageMaker Estimator parameters: '%s'",
+                     input_args.sm_estimator_parameters)
 
     estimator_kwargs = parse_estimator_kwargs(input_args.sm_estimator_parameters)
 
@@ -52,12 +75,14 @@ def run_gbconvert_job(input_args, image):
         instance_count=1,
         instance_type=instance_type,
         command=["python3"],
-        base_job_name=f"gs-gb-convert-{sm_task_name}",
+        base_job_name=sm_task_name,
         sagemaker_session=sagemaker_session,
-        tags=[{"Key":"GraphStorm", "Value":"beta"},
-            {"Key":"GraphStorm_Task", "Value":"GraphBoltConvert"}],
+        tags=[
+            {"Key": "GraphStorm", "Value": "beta"},
+            {"Key": "GraphStorm_Task", "Value": "GraphBoltConvert"},
+        ],
         env={"AWS_REGION": region},
-        **estimator_kwargs
+        **estimator_kwargs,
     )
 
     # The partition data will be download to this path on the instance
@@ -69,18 +94,23 @@ def run_gbconvert_job(input_args, image):
     # in-place and we can't write to the location where S3 files are loaded
     gb_convert_processor.run(
         code=input_args.entry_point,
-        inputs=[ProcessingInput(
-            input_name="dist_graph_s3_input",
-            destination=local_dist_part_path,
-            source=graph_data_s3,
-            s3_input_mode="File",
-        )],
+        inputs=[
+            ProcessingInput(
+                input_name="dist_graph_s3_input",
+                destination=local_dist_part_path,
+                source=graph_data_s3,
+                s3_input_mode="File",
+            )
+        ],
         arguments=[
-            "--njobs", str(input_args.gb_convert_njobs),
-            "--metadata-filename", input_args.metadata_filename,
+            "--njobs",
+            str(input_args.gb_convert_njobs),
+            "--metadata-filename",
+            input_args.metadata_filename,
         ],
         wait=not input_args.async_execution,
     )
+
 
 def get_partition_parser():
     """
@@ -90,22 +120,37 @@ def get_partition_parser():
 
     partition_args = parser.add_argument_group("GraphStorm Partition Arguments")
 
-    partition_args.add_argument("--entry-point", type=str,
+    partition_args.add_argument(
+        "--entry-point",
+        type=str,
         default="run/gb_convert_entry.py",
-        help="PATH-TO graphstorm/sagemaker/run/gb_convert_entry.py")
+        help="PATH-TO graphstorm/sagemaker/run/gb_convert_entry.py",
+    )
 
-    partition_args.add_argument("--gb-convert-njobs", type=int, default=1,
-        help="Number of parallel processes to use for GraphBolt partition conversion.")
+    partition_args.add_argument(
+        "--gb-convert-njobs",
+        type=int,
+        default=1,
+        help="Number of parallel processes to use for GraphBolt partition conversion.",
+    )
 
-    partition_args.add_argument("--metadata-filename", type=str, default="metadata.json",
+    partition_args.add_argument(
+        "--metadata-filename",
+        type=str,
+        default="metadata.json",
         help="Partition metadata file name. Default: 'metadata.json'. If GConstruct was "
-            "used should be set to <graph-name>.json")
+        "used should be set to <graph-name>.json",
+    )
 
-
-    partition_args.add_argument('--log-level', default='INFO',
-        type=str, choices=['DEBUG', 'INFO', 'WARNING', 'CRITICAL', 'FATAL'])
+    partition_args.add_argument(
+        "--log-level",
+        default="INFO",
+        type=str,
+        choices=["DEBUG", "INFO", "WARNING", "CRITICAL", "FATAL"],
+    )
 
     return parser.parse_args()
+
 
 if __name__ == "__main__":
     args = get_partition_parser()
@@ -114,8 +159,8 @@ if __name__ == "__main__":
     # NOTE: Ensure no logging has been done before setting logging configuration
     logging.basicConfig(
         level=getattr(logging, args.log_level.upper(), None),
-        format='%(asctime)s - %(levelname)s - %(message)s'
-        )
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
 
     partition_image = args.image_url
     if not args.instance_type:
--- sagemaker/run/gb_convert_entry.py
+++ sagemaker/run/gb_convert_entry.py
@@ -1,5 +1,5 @@
 """
-    Copyright 2023 Contributors
+    Copyright Contributors
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -23,20 +23,28 @@ import os
 from graphstorm.sagemaker.sagemaker_gb_convert import run_gb_convert
 
 
-if __name__ =='__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--njobs", type=int, default=1,
-                        help="Number of parallel jobs to run for partition conversion.")
-    parser.add_argument("--metadata-filename", type=str, default="metadata.json",
+    parser.add_argument(
+        "--njobs",
+        type=int,
+        default=1,
+        help="Number of parallel jobs to run for partition conversion.",
+    )
+    parser.add_argument(
+        "--metadata-filename",
+        type=str,
+        default="metadata.json",
         help="Partition metadata file name. Default: 'metadata.json'. If GConstruct was "
-            "used should be set to <graph-name>.json")
+        "used should be set to <graph-name>.json",
+    )
     args = parser.parse_args()
 
     # NOTE: Ensure no logging has been done before setting logging configuration
     logging.basicConfig(
         level=getattr(logging, "INFO", None),
-        format='%(asctime)s - %(levelname)s - %(message)s'
-        )
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
 
     # Read processing config file
     with open("/opt/ml/config/processingjobconfig.json", "r", encoding="utf-8") as f:
@@ -52,14 +60,16 @@ if __name__ =='__main__':
             break
     else:
         raise ValueError(
-            "Could not find a Processing input named 'dist_graph_s3_input'")
-
+            "Could not find a Processing input named 'dist_graph_s3_input'"
+        )
 
     # Run the conversion locally
     logging.info("Running graph partition conversion...")
     print(f"{local_partition_path=}, {s3_partition_path=}")
     run_gb_convert(
         s3_partition_path,
-        local_dist_part_config=os.path.join(local_partition_path, args.metadata_filename),
-        njobs=args.njobs
+        local_dist_part_config=os.path.join(
+            local_partition_path, args.metadata_filename
+        ),
+        njobs=args.njobs,
     )
