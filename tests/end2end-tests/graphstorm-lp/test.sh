ad9981a67ae7185cf7bdbbf1e7e5763c250de9ad
Xiang Song <xiangsx@amazon.com>
2025-01-28 21:11:33 -0800

    Fix text

--- tests/end2end-tests/graphstorm-lp/test.sh
+++ tests/end2end-tests/graphstorm-lp/test.sh
@@ -197,7 +197,7 @@ python3 -m graphstorm.run.gs_link_prediction --workspace $GS_HOME/training_scrip
 error_and_exit $?
 
 echo "**************dataset: Movielens, RGCN layer 2, node feat: fixed HF BERT & sparse embed, BERT nodes: movie, inference: full-graph, negative_sampler: joint, decoder: TransE_L1, exclude_training_targets: false, lp-embed-normalizer: l2_norm, bayesian personalized ranking loss with edge weight"
-python3 -m graphstorm.run.gs_link_prediction --workspace $GS_HOME/training_scripts/gsgnn_lp --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lp_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_lp.yaml --fanout '10,15' --num-layers 2 --use-mini-batch-infer false --lp-decoder-type transe_l1 --train-etype user,rating,movie movie,rating-rev,user --num-epochs 1 --eval-frequency 300 --lp-loss-func bpr --lp-embed-normalizer l2_norm --lp-edge-weight-for-loss rate
+python3 -m graphstorm.run.gs_link_prediction --workspace $GS_HOME/training_scripts/gsgnn_lp --num-trainers $NUM_TRAINERS --num-servers 1 --num-samplers 0 --part-config /data/movielen_100k_lp_train_val_1p_4t/movie-lens-100k.json --ip-config ip_list.txt --ssh-port 2222 --cf ml_lp.yaml --fanout '10,15' --num-layers 2 --use-mini-batch-infer false --lp-decoder-type transe_l1 --train-etype user,rating,movie --num-epochs 1 --eval-frequency 300 --lp-loss-func bpr --lp-embed-normalizer l2_norm --lp-edge-weight-for-loss rate
 
 error_and_exit $?
 
