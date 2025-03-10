cb266281d50ef31e6e73cc23af96f79a360e8ce7
Theodore Vasiloudis <thvasilo@amazon.com>
2025-03-10 13:39:40 -0700

    [Docker] Ensure hostname fix is at right place for SageMaker image. Use recommended paths for code (#1203)
    …
    
    *Issue #, if available:*
    
    *Description of changes:*
    
    * Previously our SageMaker jobs would log errors like `ERROR: ld.so:
    object '/libchangehostname.so' from LD_PRELOAD cannot be preloaded
    (cannot open shared object file): ignored.` at the beginning of the job
    because the entry point script is trying to use a library
    (changehostname.c) that gets compiled at launch time, but we were using
    the wrong path there, which meant there was no library compiled.
      * This PR fixes the path for the `changehostname.c`
    * Our containers use a custom entry point, and code under
    `/opt/ml/code/` is usually reserved to be executed straight from
    SageMaker i.e. we do
    [this](https://docs.aws.amazon.com/sagemaker/latest/dg/your-algorithms-training-algo-dockerfile.html#your-algorithms-training-algo-dockerfile-api-ep-in)
    and not
    [this](https://docs.aws.amazon.com/sagemaker/latest/dg/adapt-training-container.html#byoc-training-step2).
    The presence of the code under `/opt/ml/code/` would occasionally cause
    problems in cases like HPO.
    * This PR uses `/usr/local/bin/` as our destination for our code to
    ensure we don't modify the files that SageMaker itself creates.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: xiang song(charlie.song) <classicxsong@gmail.com>

--- docker/sagemaker/build_artifacts/start_with_right_hostname.sh
+++ docker/sagemaker/build_artifacts/start_with_right_hostname.sh
@@ -2,8 +2,8 @@
 
 if [[ "$1" = "train" ]]; then
      CURRENT_HOST=$(jq .current_host  /opt/ml/input/config/resourceconfig.json)
-     sed -ie "s/PLACEHOLDER_HOSTNAME/$CURRENT_HOST/g" changehostname.c
-     gcc -o changehostname.o -c -fPIC -Wall changehostname.c
+     sed -ie "s/PLACEHOLDER_HOSTNAME/$CURRENT_HOST/g" /usr/local/bin/changehostname.c
+     gcc -o changehostname.o -c -fPIC -Wall /usr/local/bin/changehostname.c
      gcc -o libchangehostname.so -shared -export-dynamic changehostname.o -ldl
      CWD=$(pwd)
      LD_PRELOAD=$CWD/libchangehostname.so train
