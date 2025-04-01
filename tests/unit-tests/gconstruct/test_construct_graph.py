awslabs/graphstorm

commit 8e6e985d19609b7092516f60b82e64bee3051abc

Don't print torchdata deprecation warnings (#1227)
*Issue #, if available:*

*Description of changes:*

* Currently every time DGL is imported the torchdata deprecation
warnings are printed. For logs with multiple workers and trainers we can
have the same message repeated tens of times (once per DGL process),
polluting the logs.
* This PR filters out the warning since we are aware of the deprecation
notice and it come from DGL and not graphstorm

Old behavior:

```
root@3ef2b8d262b3:/opt/ml/code# python
>>> import graphstorm
/opt/conda/lib/python3.11/site-packages/torchdata/datapipes/__init__.py:18: UserWarning:
################################################################################
WARNING!
The 'datapipes', 'dataloader2' modules are deprecated and will be removed in a
future torchdata release! Please see https://github.com/pytorch/data/issues/1196
to learn more and leave feedback.
################################################################################

  deprecation_warning()
>>>
```

new behavior:

```
root@88e89a8ea077:/opt/ml/code# python
>>> import graphstorm
>>>
```


By submitting this pull request, I confirm that you can use, modify,
copy, and redistribute this contribution, under the terms of your
choice.

python/graphstorm/__init__.py
@@ -16,6 +16,15 @@
     Graphstorm package.
 """
 __version__ = "0.4.1"
+import warnings
+
+# Don't print torchdata warnings
+warnings.filterwarnings(
+    "ignore",
+    message=".*The 'datapipes', 'dataloader2' modules are deprecated.*")
+warnings.filterwarnings(
+    "ignore",
+    category=UserWarning, module="torchdata.datapipes")
 
 from . import gsf
 from . import utils

commit afa4bf3ae2c8fc0363dbd98f869708cc80c60401

Apply suggestions for Noop transformation (#1228)
*Issue #, if available:*

*Description of changes:*

* Apply final comments from #1225 
* Also change test names for other transformations. Previously all
transformations were tested under a single unit test. This meant that if
one of the tests failed the following ones would not run which is not
the intended behavior when testing, we don't want tests to depend on
each other. This also allows the individual tests to be discoverable and
testable within an IDE.
* Also fixes a pylint error that was introduced in #1227 


By submitting this pull request, I confirm that you can use, modify,
copy, and redistribute this contribution, under the terms of your
choice.

graphstorm-processing/graphstorm_processing/config/config_conversion/gconstruct_converter.py
@@ -227,10 +227,11 @@ class GConstructConfigConverter(ConfigConverter):
                         }
                 elif gconstruct_transform_dict["name"] == "no-op":
                     gsp_transformation_dict["name"] = "no-op"
+                    gsp_transformation_dict["kwargs"] = {}
                     if "separator" in gconstruct_transform_dict:
-                        gsp_transformation_dict["kwargs"] = {
-                            "separator": gconstruct_transform_dict["separator"],
-                        }
+                        gsp_transformation_dict["kwargs"]["separator"] = gconstruct_transform_dict[
+                            "separator"
+                        ]
                     if "truncate_dim" in gconstruct_transform_dict:
                         gsp_transformation_dict["kwargs"]["truncate_dim"] = (
                             gconstruct_transform_dict["truncate_dim"]

python/graphstorm/__init__.py
@@ -15,6 +15,7 @@
 
     Graphstorm package.
 """
+# pylint: disable=wrong-import-position
 __version__ = "0.4.1"
 import warnings
 

python/graphstorm/gconstruct/transform.py
@@ -24,7 +24,7 @@ import abc
 import json
 import warnings
 from abc import ABC, abstractmethod
-from numbers import Number
+from numbers import Integral
 from typing import Any, Dict, List, Optional
 
 import numpy as np
@@ -1070,7 +1070,7 @@ class Noop(FeatTransform):
         .. versionadded:: 0.5.0
     """
     def __init__(self, col_name, feat_name, out_dtype=None,
-                 truncate_dim=None, separator: Optional[str]=None):
+                 truncate_dim: Optional[int]=None, separator: Optional[str]=None):
         # Ensure self.out_dtype has a value
         out_dtype = np.float32 if out_dtype is None else out_dtype
         super(Noop, self).__init__(col_name, feat_name, out_dtype)
@@ -1102,11 +1102,8 @@ class Noop(FeatTransform):
             # using vectorized numpy op
             feats = np.char.split(feats, sep=self.separator)
 
-            # Convert to regular Python list
-            feats_list = feats.tolist()
-
-            # Convert lists of str to lists of float, this is faster than using numpy
-            feats = [[float(x) for x in sublist] for sublist in feats_list]
+            # Convert array of str to lists of float, this is faster than using numpy
+            feats = [[float(x) for x in sublist] for sublist in feats]
 
             # Convert back to numpy
             feats = np.array(feats, dtype=self.out_dtype)
@@ -1115,13 +1112,11 @@ class Noop(FeatTransform):
                 f"The feature {self.feat_name} has to be NumPy array."
         assert np.issubdtype(feats.dtype, np.integer) \
                 or np.issubdtype(feats.dtype, np.floating), \
-                f"The feature {self.feat_name} has to be integers or floats."
+                f"The feature {self.feat_name} has to be integers or floats, got '{feats.dtype}'."
 
         if validate_features():
             assert validate_numerical_feats(feats), \
                 f"There are NaN, Inf or missing value in the {self.feat_name} feature."
-
-
 
         if self.truncate_dim is not None:
             if isinstance(feats, np.ndarray):
@@ -1130,7 +1125,6 @@ class Noop(FeatTransform):
                 assert isinstance(feats, ExtMemArrayWrapper)
                 # Need to convert to in-memory array to make truncation possible
                 feats = feats.to_numpy()[:, :self.truncate_dim]
-
 
         return {self.feat_name: feats}
 
@@ -1444,9 +1438,13 @@ def parse_feat_ops(confs, input_data_format=None):
                                                          separator=separator)
             elif conf['name'] == 'no-op':
                 if 'separator' in conf:
-                    assert isinstance(conf['separator'], str)
+                    assert isinstance(conf['separator'], str), \
+                        f"no-op 'separator' needs to be a string got {type(conf['separator'])}"
                 if 'truncate_dim' in conf:
-                    assert isinstance(conf['truncate_dim'], Number)
+                    assert isinstance(conf['truncate_dim'], Integral), (
+                        f"no-op 'truncate_dim' needs to be an integer, "
+                        f"got {type(conf['truncate_dim'])}"
+                    )
                 transform = Noop(
                     feat['feature_col'],
                     feat_name,

tests/unit-tests/gconstruct/test_construct_graph.py
@@ -199,7 +199,7 @@ def test_hdf5():
 
     os.remove(tmpfile)
 
-def check_feat_ops_noop():
+def test_feat_ops_noop():
     # Just get the features without transformation.
     feat_op1 = [{
         "feature_col": "test1",
@@ -229,13 +229,27 @@ def check_feat_ops_noop():
     assert res[0].feat_name == feat_op1[0]["feature_col"]
     assert isinstance(res[0], Noop)
 
+    # When the transform name is specified.
+    feat_op1 = [{
+        "feature_col": "test1",
+        "transform": {
+            "name": "no-op",
+        }
+    }]
+    (res, _, _, _) = parse_feat_ops(feat_op1)
+    assert len(res) == 1
+    assert res[0].col_name == feat_op1[0]["feature_col"]
+    assert res[0].feat_name == feat_op1[0]["feature_col"]
+    assert isinstance(res[0], Noop)
+
 def test_noop_string():
     text_vector_data = {
-        "test2": ["1;2;3", "4;5;6"]
+        "test2": np.array(["1;2;3", "4;5;6"], dtype=object)
     }
+    feature_name = "test3"
     text_vector_config = [{
         "feature_col": "test2",
-        "feature_name": "test3",
+        "feature_name": feature_name,
         "transform": {
             "name": "no-op",
             "separator": ";"
@@ -248,10 +262,44 @@ def test_noop_string():
     assert noop_transform.separator == ";"
 
     vector_data_processed = process_features(text_vector_data, res)
+    expected_array = np.array([[1, 2, 3], [4, 5, 6]])
 
-    assert_equal(vector_data_processed["test3"], np.array([[1, 2, 3], [4, 5, 6]]))
+    assert_equal(vector_data_processed[feature_name], expected_array)
 
-def check_feat_ops_tokenize():
+    # Test providing the separator + truncate_dim
+    text_vector_config = [{
+        "feature_col": "test2",
+        "feature_name": feature_name,
+        "transform": {
+            "name": "no-op",
+            "separator": ";",
+            "truncate_dim": 2
+        },
+    }]
+    (res, _, _, _) = parse_feat_ops(text_vector_config)
+    expected_array = np.array([[1, 2], [4, 5]])
+    vector_data_processed = process_features(text_vector_data, res)
+
+    assert_equal(vector_data_processed[feature_name], expected_array)
+
+    # Test when the separator is not specified but input is strings
+    text_vector_config = [{
+        "feature_col": "test2",
+        "feature_name": feature_name,
+        "transform": {
+            "name": "no-op",
+        },
+    }]
+    (res, _, _, _) = parse_feat_ops(text_vector_config)
+
+    with pytest.raises(
+        AssertionError,
+        match=f"The feature {feature_name} has to be integers or floats, got 'object'."
+    ):
+        process_features(text_vector_data, res)
+
+
+def test_feat_ops_tokenize():
     feat_op2 = [
         {
             "feature_col": "test1",
@@ -293,7 +341,7 @@ def check_feat_ops_tokenize():
     assert "attention_mask" in proc_res
     assert "token_type_ids" in proc_res
 
-def check_feat_ops_bert():
+def test_feat_ops_bert():
     feat_op3 = [
         {
             "feature_col": "test3",
@@ -339,7 +387,7 @@ def check_feat_ops_bert():
     assert len(proc_res2['test4']) == 2
     np.testing.assert_allclose(proc_res['test4'], proc_res2['test4'], rtol=1e-3)
 
-def check_feat_ops_maxmin():
+def test_feat_ops_maxmin():
     data0 = {
         "test1": np.random.rand(4, 2),
     }
@@ -434,7 +482,7 @@ def check_feat_ops_maxmin():
     assert_almost_equal(proc_res6[:,0], data_col0)
     assert_almost_equal(proc_res6[:,1], data_col1)
 
-def check_feat_ops_rank_gauss():
+def test_feat_ops_rank_gauss():
     data7_0 = {
         "test1": np.random.randn(100,2).astype(np.float32)
     }
@@ -500,7 +548,7 @@ def check_feat_ops_rank_gauss():
     num_unique_feat = np.unique(trans_feat, axis=0)
     assert len(num_unique_feat) == 2
 
-def check_feat_ops_categorical():
+def test_feat_ops_categorical():
     feat_op7 = [
         {
             "feature_col": "test1",
@@ -565,13 +613,6 @@ def check_feat_ops_categorical():
         assert multi_hot[int(str_i2)] == 1
     assert 'mapping' in feat_op8[0]["transform"]
 
-def test_feat_ops():
-    check_feat_ops_noop()
-    check_feat_ops_tokenize()
-    check_feat_ops_bert()
-    check_feat_ops_maxmin()
-    check_feat_ops_categorical()
-    check_feat_ops_rank_gauss()
 
 def test_process_features_fp16():
     np.random.seed(1)