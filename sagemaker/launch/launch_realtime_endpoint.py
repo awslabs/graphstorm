c2b497c470a2032ded0bf224d7f7997bfbd532df
Ubuntu <ubuntu@ip-172-31-11-9.us-west-2.compute.internal>
2025-05-08 05:28:40 +0000

    fix some lints

--- sagemaker/launch/launch_realtime_endpoint.py
+++ sagemaker/launch/launch_realtime_endpoint.py
@@ -20,15 +20,14 @@ import os
 import argparse
 import logging
 import tempfile
-import boto3 # pylint: disable=import-error
-import sagemaker as sm
 from time import gmtime, strftime
+import boto3 # pylint: disable=import-error
 from botocore.exceptions import WaiterError
-
 from launch_utils import (wrap_model_artifacts,
                           check_tarfile_s3_object,
                           upload_data_to_s3,
                           check_name_format)
+import sagemaker as sm
 
 
 # TODO: When adding new realtime inference tasks, modify this list
@@ -60,13 +59,13 @@ def run_job(input_args):
     # prepare sessions
     b3_session = boto3.Session(region_name=input_args.region)
     sm_session = sm.Session(boto_session=b3_session)
-    
+
     # If users provide the S3 tar file path already, directly use it for model creation
     if input_args.model_tarfile_s3:
         entrypoint_file_name = input_args.entrypoint_file_name
 
         model_url_s3 = input_args.model_tarfile_s3
-        assert check_tarfile_s3_object(args.model_tarfile_s3) == True, \
+        assert check_tarfile_s3_object(args.model_tarfile_s3) is True, \
             (f'Not find a tar file in the given S3 URL: {args.model_tarfile_s3}...')
 
         # model_name is either directly given by users, or had been assigned by the argument
@@ -93,12 +92,12 @@ def run_job(input_args):
             model_tarfile_path = wrap_model_artifacts(path_to_model, path_to_yaml, path_to_json,
                                                     path_to_entry, tmpdirname,
                                                     output_tarfile_name=model_name)
-            logging.info('Packed and compressed model artifacts into {}.'.format(model_tarfile_path))
+            logging.info('Packed and compressed model artifacts into %.', model_tarfile_path)
 
             # upload the model tar file to the given S3 bucket
             model_url_s3 = upload_data_to_s3(input_args.upload_tarfile_s3, model_tarfile_path,
                                             sm_session)
-            logging.info('Uploaded the model tar file to {}'.format(model_url_s3))
+            logging.info('Uploaded the model tar file to %s.', model_url_s3)
 
     # ================= create deployable model ================= #
     image_url = input_args.image_url
@@ -114,7 +113,7 @@ def run_job(input_args):
     sm_model_name = model_name + '-' + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
     create_model_response = sm_client.create_model(ModelName=sm_model_name,
                                                    ExecutionRoleArn=role, Containers=[container])
-    logging.info('Model ARN: {}'.format(create_model_response['ModelArn']))
+    logging.info('Model ARN: %s', create_model_response['ModelArn'])
 
     # ================= create an endpoint configuration ================= #
     sm_ep_config_name = model_name + "-EndpointConfig-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
@@ -131,8 +130,8 @@ def run_job(input_args):
                 }
             ],
         )
-
-    logging.info("Endpoint config Arn: {}".format(create_endpoint_config_response["EndpointConfigArn"]))
+    endpoint_arn = create_endpoint_config_response["EndpointConfigArn"]
+    logging.info("Endpoint config Arn: %s", endpoint_arn)
 
     # ================= create an endpoint ================= #
     sm_ep_name = model_name + "-Endpoint-" + strftime("%Y-%m-%d-%H-%M-%S", gmtime())
@@ -140,25 +139,25 @@ def run_job(input_args):
     create_endpoint_response = sm_client.create_endpoint(
         EndpointName=sm_ep_name, EndpointConfigName=sm_ep_config_name
     )
-    logging.info("Endpoint Arn: {}".format(create_endpoint_response["EndpointArn"]))
+    logging.info("Endpoint Arn: %s", create_endpoint_response["EndpointArn"])
 
     resp = sm_client.describe_endpoint(EndpointName=sm_ep_name)
     status = resp["EndpointStatus"]
-    logging.info("Endpoint Status: " + status)
+    logging.info("Endpoint Status: %s", status)
 
-    logging.info("Waiting for {} endpoint to be in service...".format(sm_ep_name))
+    logging.info("Waiting for %s endpoint to be in service...", sm_ep_name)
     waiter = sm_client.get_waiter("endpoint_in_service")
-    
+
     try:
         waiter.wait(EndpointName=sm_ep_name,
                     WaiterConfig={
                         'Delay': 30,        # seconds between querying
                         'MaxAttempts': 60   # max retries (~30 minutes here)
                     })
-        logging.info('{} endpoint has been successfully created, and ready to be \
-                 invoked!'.format(sm_ep_name))
+        logging.info('%s endpoint has been successfully created, and ready to be \
+                 invoked!', sm_ep_name)
     except WaiterError as e:
-        logging.error("Waiter timed out or endpoint creation failed:", e)
+        logging.error("Waiter timed out or endpoint creation failed: %s", e)
 
 
 def get_realtime_infer_parser():
@@ -193,11 +192,11 @@ def get_realtime_infer_parser():
         help="The S3 location for uploading the packed model artifacts tar file.")
     realtime_infer_parser.add_argument("--infer-task-type", type=str,
         choices=SUPPORTED_REALTIME_INFER_TASKS,
-        help=(f"The name of real time inference task. Options \
-               include {SUPPORTED_REALTIME_INFER_TASKS}"))
+        help=f"The name of real time inference task. Options \
+               include {SUPPORTED_REALTIME_INFER_TASKS}")
     realtime_infer_parser.add_argument("--model-name", type=check_name_format,
         default='GSF-Model4Realtime',
-        help="The name for the to-be created SageMaker objects. The name should follow \
+        help=r"The name for the to-be created SageMaker objects. The name should follow \
               a regular expression pattern: ^[a-zA-Z0-9]([\-a-zA-Z0-9]*[a-zA-Z0-9])$. Default \
               is \"GSF-Model4Realtime\".")
 
@@ -211,7 +210,7 @@ def get_realtime_infer_parser():
 
     return realtime_infer_parser
 
-def validate_realtime_infer_arguments(args):
+def validate_realtime_infer_arguments(input_args):
     """ Validate the input arguments with the designed launch logic
     
     The real-time inference endpoint lauch has the logic as the followings.
@@ -228,31 +227,31 @@ def validate_realtime_infer_arguments(args):
        --upload-tarfile-s3
     3. If users also provide a model name in the --model-name argument, the script will use it and
        check if it follows the SageMaker naming fomat.
-       
-       Note: Checking model_name format actually happens in the argument parsing stage. So there is no
-              such check in this function.
-    4. TODO(James): Will do sanity check of the contents of the given YAML, and JSON files when they
-                    become available.
+
+       Note: Checking model_name format actually happens in the argument parsing stage. So there
+             is no such check in this function.
+    4. TODO(James): Will do sanity check of the contents of the given YAML, and JSON files when
+                    they become available.
 
     """
-    if args.model_tarfile_s3:
-        assert args.entrypoint_file_name, ('To use your own model tar file, please set the \
+    if input_args.model_tarfile_s3:
+        assert input_args.entrypoint_file_name, ('To use your own model tar file, please set the \
             --entrypoint-file-name argument, and place the actual file into a subfolder, \
             named \'/code\', inside the tar file, as requested in the SageMaker document: \
             https://sagemaker.readthedocs.io/en/stable/frameworks/pytorch/using_pytorch.html#model-directory-structure.')
     else:
-        assert args.restore_model_path, f'To use GraphStorm default real-time inference ' + \
+        assert input_args.restore_model_path, 'To use GraphStorm default real-time inference ' + \
                      'endpoint launch script, please set --restore-model-path argument.'
-        assert args.model_yaml_config_file, f'To use GraphStorm default real-time ' + \
+        assert input_args.model_yaml_config_file, 'To use GraphStorm default real-time ' + \
                      'inference endpoint launch script, please set --model-yaml-config-file ' + \
                      'argument.'
-        assert args.graph_json_config_file, f'To use GraphStorm default real-time ' + \
+        assert input_args.graph_json_config_file, 'To use GraphStorm default real-time ' + \
                      'inference endpoint launch script, please set --graph-json-config-file ' + \
                      'argument.'
-        assert args.upload_tarfile_s3, f'To use GraphStorm default real-time ' + \
+        assert input_args.upload_tarfile_s3, 'To use GraphStorm default real-time ' + \
                      'inference endpoint launch script, please set --upload-tarfile-s3 ' + \
                      'argument.'
-        assert args.infer_task_type, f'To use GraphStorm default real-time ' + \
+        assert input_args.infer_task_type, 'To use GraphStorm default real-time ' + \
                      'inference endpoint launch script, please set --infer-task-type ' + \
                      'argument.'
 
