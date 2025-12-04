9b8eb9195fcbc43e188cbf7e5cdcb5ce7b6800cb
Ubuntu <ubuntu@ip-172-31-60-50.us-west-2.compute.internal>
2025-12-04 17:59:05 +0000

    add test update_ops

--- tests/unit-tests/gconstruct/test_transform.py
+++ tests/unit-tests/gconstruct/test_transform.py
@@ -26,11 +26,13 @@ import graphstorm as gs
 from graphstorm.gconstruct.transform import (
     parse_feat_ops,
     process_features,
-    preprocess_features
+    preprocess_features,
+    update_ops
 )
 from graphstorm.gconstruct.transform import (_get_output_dtype,
                                              NumericalMinMaxTransform,
                                              NumericalStandardTransform,
+                                             GlobalProcessFeatTransform,
                                              Noop,
                                              RankGaussTransform,
                                              CategoricalTransform,
@@ -1452,3 +1454,41 @@ def test_hf_embedding(bert_model="bert-base-uncased"):
             hf_emb[idx], expected_output[idx], decimal=3, err_msg=f"Row {idx} is not equal"
         )
         
+
+def test_update_ops():
+    """ test the update operation function
+    """
+    np.random.seed(0)
+    feats0 = np.random.randn(100)
+    feats0 = feats0 + 1
+    feats1 = np.random.randn(100)
+    feats1 = feats1 + 1
+    feats2 = np.random.randn(100)
+    feats2 = feats2 + 1
+    
+    # only feats0 and feats2 are in the input feature dict
+    input_feats = {
+        'feat0': feats0,
+        'feat2': feats2
+    }
+    # all three features have transforms in construction json
+    ops = [
+        NumericalStandardTransform("feat0", "feat0"),
+        GlobalProcessFeatTransform("feat1", "feat1", np.float32),
+        NumericalMinMaxTransform("feat2", "feat2")
+    ]
+
+    # run update_ops
+    new_feat_ops, two_phase_feat_ops, after_merge_feat_ops, hard_edge_neg_ops = \
+        update_ops(ops, input_feats)
+    
+    # new ops list should only have two ops
+    assert len(new_feat_ops) == 2
+    assert len(two_phase_feat_ops) == 2
+    assert len(after_merge_feat_ops) == 0
+    assert len(hard_edge_neg_ops) == 0
+    # new ops list should only have feat0 and feat2
+    assert all([op.feat_name in ['feat0', 'feat2'] for op in new_feat_ops])
+
+if __name__ == '__main__':
+    test_update_ops()
